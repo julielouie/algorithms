cc48d9d9c0302a478405d364edbba1bc242db3c3
Julie Chung <juliehchung96@gmail.com>
2020-02-03 15:58:01 -0800

    Computing reversed lists

--- add-two-numbers/main.js
+++ add-two-numbers/main.js
@@ -1,3 +1,6 @@
 const addTwoNumbers = function (l1, l2) {
-
+  const link1 = parseInt(l1.reverse().join(''));
+  const link2 = parseInt(l2.reverse().join(''));
+  const resLink = link1 + link2 + '';
+  return resLink.split('');
 };
