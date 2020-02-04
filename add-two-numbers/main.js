8b683638547aa5a05f5128dd8b36a3b5782da0fd
Julie Chung <juliehchung96@gmail.com>
2020-02-03 18:12:13 -0800

    Creating function to allow addition functionality

--- add-two-numbers/main.js
+++ add-two-numbers/main.js
@@ -1,3 +1,29 @@
-const addTwoNumbers = function (l1, l2) {
+/*
+Definition for singly-linked list.
 
+@param {ListNode} l1
+@param {ListNode} l2
+@return {ListNode}
+*/
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+const addTwoNumbers = function (l1, l2) {
+  const head = new ListNode(0);
+  let point = head;
+  let append = 0;
+  const run = function (l1, l2, point) {
+    if (l1 === null && l2 === null) {
+      if (append == 1) {
+        point.next = new ListNode(1);
+      }
+      return head.next;
+    }
+
+
+  };
+  return run(l1, l2, point)
 };
