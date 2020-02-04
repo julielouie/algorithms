cf50333738bf737aeafda943979f01c2c1f0d1cd
Julie Chung <juliehchung96@gmail.com>
2020-02-03 16:29:38 -0800

    Starting over due to misrepresentation of linked lists

--- add-two-numbers/main.js
+++ add-two-numbers/main.js
@@ -1,6 +1,3 @@
 const addTwoNumbers = function (l1, l2) {
-  const link1 = parseInt(l1.reverse().join(''));
-  const link2 = parseInt(l2.reverse().join(''));
-  const resLink = link1 + link2 + '';
-  return resLink.split('');
+
 };
