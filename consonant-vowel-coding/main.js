7b8a35656945af95c1b63ba3d3dc9a6abbb5c33d
Julie Chung <juliehchung96@gmail.com>
2020-02-12 14:00:21 -0800

    Working on adding numbers as strings to separate arrays per word, considering spacing

--- consonant-vowel-coding/main.js
+++ consonant-vowel-coding/main.js
@@ -1,9 +1,11 @@
 function stringCode(sentence) {
   const separatedLowCase = sentence.toLowerCase().split(' ');
   const splitLetters = separatedLowCase.map(element => element.split(''));
-  let consCount = 0;
-  let vowCount = 0;
+  const consonants = [];
+  const vowels = [];
   const counting = splitLetters.map(element => {
+    let consCount = 0;
+    let vowCount = 0;
     for (let index = 0; index < element.length; index++) {
       if (element !== 'a' && element !== 'e' && element !== 'i' && element !== 'o' && element !== 'u') {
         consCount++;
@@ -11,5 +13,7 @@ function stringCode(sentence) {
         vowCount++;
       }
     }
+    consonants.push(`${consCount}`);
+    vowels.push(`${vowCount}`);
   })
 }
