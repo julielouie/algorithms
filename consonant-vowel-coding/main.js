aef124b53b84b086a20ddb6cf6537ceb75259763
Julie Chung <juliehchung96@gmail.com>
2020-02-12 13:56:05 -0800

    Continuing stringCode challenge

--- consonant-vowel-coding/main.js
+++ consonant-vowel-coding/main.js
@@ -1,3 +1,15 @@
 function stringCode(sentence) {
-
+  const separatedLowCase = sentence.toLowerCase().split(' ');
+  const splitLetters = separatedLowCase.map(element => element.split(''));
+  let consCount = 0;
+  let vowCount = 0;
+  const counting = splitLetters.map(element => {
+    for (let index = 0; index < element.length; index++) {
+      if (element !== 'a' && element !== 'e' && element !== 'i' && element !== 'o' && element !== 'u') {
+        consCount++;
+      } else if (element === 'a' || element === 'e' || element === 'i' || element === 'o' || element === 'u') {
+        vowCount++;
+      }
+    }
+  })
 }
