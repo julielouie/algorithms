a871891a93b6b5131c314ef488d975c63ea56d2c
Julie Chung <juliehchung96@gmail.com>
2020-02-12 14:04:40 -0800

    Code completed, testing to start

--- consonant-vowel-coding/main.js
+++ consonant-vowel-coding/main.js
@@ -1,9 +1,9 @@
 function stringCode(sentence) {
   const separatedLowCase = sentence.toLowerCase().split(' ');
   const splitLetters = separatedLowCase.map(element => element.split(''));
-  const consonants = [];
-  const vowels = [];
-  const counting = splitLetters.map(element => {
+  let consonants = [];
+  let vowels = [];
+  splitLetters.forEach(element => {
     let consCount = 0;
     let vowCount = 0;
     for (let index = 0; index < element.length; index++) {
@@ -15,5 +15,8 @@ function stringCode(sentence) {
     }
     consonants.push(`${consCount}`);
     vowels.push(`${vowCount}`);
-  })
+  });
+  consonants = consonants.join(' ');
+  vowels = vowels.join(' ');
+  return [consonants, vowels];
 }
