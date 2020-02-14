cf5d33a5f68e1fe45af16cf05b4c7be316f9cdf6
Julie Chung <juliehchung96@gmail.com>
2020-02-14 10:52:32 -0800

    Completed challenge, starting testing

--- seven-boom/main.js
+++ seven-boom/main.js
@@ -1,3 +1,8 @@
 function sevenBoom(arr) {
-
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] == 7) {
+      return 'Boom!';
+    }
+  }
+  return 'there is no 7 in the array';
 }
