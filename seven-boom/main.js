56787977fb518be17cbd7ee3b6f2f8148cca63f2
Julie Chung <juliehchung96@gmail.com>
2020-02-14 10:55:29 -0800

    Testing solution

--- seven-boom/main.js
+++ seven-boom/main.js
@@ -6,3 +6,10 @@ function sevenBoom(arr) {
   }
   return 'there is no 7 in the array';
 }
+
+const sevenArr = [1, 2, 3, 4, 5, 6, 7];
+const noSevenArr = [1, 2, 3, 4, 5, 6, 8];
+const sevenArrStr = ['1', '2', '3', '4', '5', '6', '7'];
+const noSevenStr = ['1', '2', '3', '4', '5', '6', '8'];
+
+console.log(sevenBoom(noSevenStr));
