879910d3726f18fcf3480c206fb6a38066ac83d9
Julie Chung <juliehchung96@gmail.com>
2020-02-15 22:55:56 +0000

    Fixed return value to include concatenated sentence with results

--- fractions-rounding/main.js
+++ fractions-rounding/main.js
@@ -2,7 +2,8 @@ function fracRound(frac, n) {
   const indexofSlash = frac.indexOf('/');
   const num1 = parseFloat(frac.slice(0, indexofSlash));
   const num2 = parseFloat(frac.slice(indexofSlash + 1));
-  return parseFloat((num1 / num2).toFixed(n));
+  const result = (num1 / num2).toFixed(n);
+  return `${frac} rounded to ${n} decimal places is ${result}`;
 }
 
 console.log(fracRound("2/8", 5));
