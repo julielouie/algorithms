6389e9f354cc30d7d94bf471c018f0fcbcb87eb2
Julie Chung <juliehchung96@gmail.com>
2020-02-15 22:51:18 +0000

    Bug fixing to find correct indices

--- fractions-rounding/main.js
+++ fractions-rounding/main.js
@@ -1,6 +1,8 @@
 function fracRound(frac, n) {
   const indexofSlash = frac.indexOf('/');
-  const num1 = parseFloat(frac.slice(frac[0], indexofSlash));
-  const num2 = parseFloat(frac.slice(indexofSlash, frac.length));
-  return (num1 / num2).toFixed(n);
+  const num1 = frac.slice(0, indexofSlash);
+  const num2 = frac.slice(indexofSlash + 1);
+  // return (num1 / num2).toFixed(n);
 }
+
+console.log(fracRound("1/3", 5));
