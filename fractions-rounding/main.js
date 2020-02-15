fa24f5ed263b4f82428e5fe99a6020790663f548
Julie Chung <juliehchung96@gmail.com>
2020-02-15 22:48:04 +0000

    First attempt completed, starting testing

--- fractions-rounding/main.js
+++ fractions-rounding/main.js
@@ -1,3 +1,6 @@
 function fracRound(frac, n) {
-
+  const indexofSlash = frac.indexOf('/');
+  const num1 = parseFloat(frac.slice(frac[0], indexofSlash));
+  const num2 = parseFloat(frac.slice(indexofSlash, frac.length));
+  return (num1 / num2).toFixed(n);
 }
