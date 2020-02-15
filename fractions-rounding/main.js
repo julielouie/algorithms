1ebf0f2f0f7698d066c25942c7efdea40b512474
Julie Chung <juliehchung96@gmail.com>
2020-02-15 22:53:53 +0000

    Fixed bug where number remained a string

--- fractions-rounding/main.js
+++ fractions-rounding/main.js
@@ -2,7 +2,7 @@ function fracRound(frac, n) {
   const indexofSlash = frac.indexOf('/');
   const num1 = parseFloat(frac.slice(0, indexofSlash));
   const num2 = parseFloat(frac.slice(indexofSlash + 1));
-  return (num1 / num2).toFixed(n);
+  return parseFloat((num1 / num2).toFixed(n));
 }
 
 console.log(fracRound("2/8", 5));
