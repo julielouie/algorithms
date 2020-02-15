24fb1ac292b9c42786a82fc2acd8271951d4b034
Julie Chung <juliehchung96@gmail.com>
2020-02-15 22:52:25 +0000

    Completed first round of ug fixing

--- fractions-rounding/main.js
+++ fractions-rounding/main.js
@@ -1,8 +1,8 @@
 function fracRound(frac, n) {
   const indexofSlash = frac.indexOf('/');
-  const num1 = frac.slice(0, indexofSlash);
-  const num2 = frac.slice(indexofSlash + 1);
-  // return (num1 / num2).toFixed(n);
+  const num1 = parseFloat(frac.slice(0, indexofSlash));
+  const num2 = parseFloat(frac.slice(indexofSlash + 1));
+  return (num1 / num2).toFixed(n);
 }
 
-console.log(fracRound("1/3", 5));
+console.log(fracRound("2/8", 5));
