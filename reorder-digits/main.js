d763c0e889b82ab316047dc0333a6f26382ffc4f
Julie Chung <juliehchung96@gmail.com>
2020-02-17 13:59:22 -0800

    Fixed bug for wrong placement of end to map method

--- reorder-digits/main.js
+++ reorder-digits/main.js
@@ -1,31 +1,27 @@
 function reorderDigits(arr, direction) {
   let resultArray;
   if (direction === 'asc') {
-    resultArray = arr.map(element => {
-      const strElem = element.toString();
-      for (let index = 0; index < strElem.length; index++) {
-      for (let index2 = 0; index2 < strElem.length; index2++) {
-        if (strElem[index2] > strElem[index2 + 1]) {
-          let temp = strElem[index2];
-          strElem[index2] = strElem[index2 + 1];
-          strElem[index2 + 1] = temp;
+    resultArray = arr.map(element => element.toString());
+      for (let index = 0; index < resultArray.length; index++) {
+      for (let index2 = 0; index2 < resultArray.length; index2++) {
+        if (resultArray[index2] > resultArray[index2 + 1]) {
+          let temp = resultArray[index2];
+          resultArray[index2] = resultArray[index2 + 1];
+          resultArray[index2 + 1] = temp;
         }
       }
     }
-    });
   } else if (direction === 'desc') {
-    resultArray = arr.map(element => {
-      const strElem = element.toString();
-      for (let index = 0; index < strElem.length; index++) {
-        for (let index2 = 0; index2 < strElem.length; index2++) {
-          if (strElem[index2] < strElem[index2 + 1]) {
-            let temp = strElem[index2];
-            strElem[index2] = strElem[index2 + 1];
-            strElem[index2 + 1] = temp;
+    resultArray = arr.map(element => element.toString());
+      for (let index = 0; index < resultArray.length; index++) {
+        for (let index2 = 0; index2 < resultArray.length; index2++) {
+          if (resultArray[index2] < resultArray[index2 + 1]) {
+            let temp = resultArray[index2];
+            resultArray[index2] = resultArray[index2 + 1];
+            resultArray[index2 + 1] = temp;
           }
         }
       }
-    });
   }
   return resultArray;
 }
