e7b7545a88486a70d1a4b0fa0b288de1c685c547
Julie Chung <juliehchung96@gmail.com>
2020-02-17 15:03:20 -0800

    Fixed code organization to allow functionality

--- reorder-digits/main.js
+++ reorder-digits/main.js
@@ -1,7 +1,7 @@
 function reorderDigits(arr, direction) {
   if (direction === 'asc') {
     for (let originalIndex = 0; originalIndex < arr.length; originalIndex++) {
-      const currentNum = arr[originalIndex].toString();
+      let currentNum = arr[originalIndex].toString().split('');
       for (let index = 0; index < currentNum.length; index++) {
         for (let index2 = 0; index2 < currentNum.length; index2++) {
           if (currentNum[index2] > currentNum[index2 + 1]) {
@@ -11,10 +11,11 @@ function reorderDigits(arr, direction) {
           }
         }
       }
+      arr[originalIndex] = parseFloat(currentNum.join(''));
     }
   } else if (direction === 'desc') {
     for (let originalIndex = 0; originalIndex < arr.length; originalIndex++) {
-      const currentNum = arr[originalIndex].toString();
+      let currentNum = arr[originalIndex].toString();
       for (let index = 0; index < currentNum.length; index++) {
         for (let index2 = 0; index2 < currentNum.length; index2++) {
           if (currentNum[index2] < currentNum[index2 + 1]) {
@@ -29,5 +30,5 @@ function reorderDigits(arr, direction) {
   return arr;
 }
 
-const array = [155, 134, 89, 44, 112];
+const array = [515, 341, 98, 44, 211];
 console.log(reorderDigits(array, 'asc'));
