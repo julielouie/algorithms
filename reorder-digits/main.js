c4119633e3c3d7cdfed05cfab5dbb094d8c97baa
Julie Chung <juliehchung96@gmail.com>
2020-02-17 13:55:25 -0800

    Completed first attempt of challenge, starting testing

--- reorder-digits/main.js
+++ reorder-digits/main.js
@@ -1,3 +1,31 @@
 function reorderDigits(arr, direction) {
-
+  let resultArray;
+  if (direction === 'asc') {
+    resultArray = arr.map(element => {
+      const strElem = element.toString();
+      for (let index = 0; index < strElem.length; index++) {
+      for (let index2 = 0; index2 < strElem.length; index2++) {
+        if (strElem[index2] > strElem[index2 + 1]) {
+          let temp = strElem[index2];
+          strElem[index2] = strElem[index2 + 1];
+          strElem[index2 + 1] = temp;
+        }
+      }
+    }
+    });
+  } else if (direction === 'desc') {
+    resultArray = arr.map(element => {
+      const strElem = element.toString();
+      for (let index = 0; index < strElem.length; index++) {
+        for (let index2 = 0; index2 < strElem.length; index2++) {
+          if (strElem[index2] > strElem[index2 + 1]) {
+            let temp = strElem[index2];
+            strElem[index2] = strElem[index2 + 1];
+            strElem[index2 + 1] = temp;
+          }
+        }
+      }
+    });
+  }
+  return resultArray;
 }
