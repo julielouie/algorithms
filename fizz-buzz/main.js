0d2fc07303796d5ee4ff67bde2fc505a0c1686c4
Julie Chung <juliehchung96@gmail.com>
2020-02-18 11:40:06 -0800

    Completed challenge, added test success

--- fizz-buzz/main.js
+++ fizz-buzz/main.js
@@ -1,14 +1,16 @@
 const fizzBuzz = function (n) {
   const num = n + 1;
+  let result = [];
   for (let index = 1; index < num; index++) {
     if (index % 15 === 0) {
-      console.log('FizzBuzz');
+      result.push('FizzBuzz');
     } else if (index % 3 === 0) {
-      console.log('Fizz');
+      result.push('Fizz');
     } else if (index % 5 === 0) {
-      console.log('Buzz');
+      result.push('Buzz');
     } else {
-      console.log(index);
+      result.push(`${index}`);
     }
   }
+  return result;
 }
