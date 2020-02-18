087c558e580584ae06dfef271f9a84118c4da714
Julie Chung <juliehchung96@gmail.com>
2020-02-18 11:30:09 -0800

    Completed first iteration of challenge

--- fizz-buzz/main.js
+++ fizz-buzz/main.js
@@ -1,3 +1,16 @@
 const fizzBuzz = function (n) {
-
+  const num = n + 1;
+  for (let index = 1; index < num; index++) {
+    if (index % 15) {
+      console.log('FizzBuzz');
+    } else if (index % 3) {
+      console.log('Fizz');
+    } else if (index % 5) {
+      console.log('Buzz');
+    } else {
+      console.log(index);
+    }
+  }
 };
+
+fizzBuzz(15);
