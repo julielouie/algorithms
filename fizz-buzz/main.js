12e201a6cb169497839ac3b6cfcdc9c3b6e1d32f
Julie Chung <juliehchung96@gmail.com>
2020-02-18 11:30:40 -0800

    Fixed bug for missing comparison

--- fizz-buzz/main.js
+++ fizz-buzz/main.js
@@ -1,11 +1,11 @@
 const fizzBuzz = function (n) {
   const num = n + 1;
   for (let index = 1; index < num; index++) {
-    if (index % 15) {
+    if (index % 15 === 0) {
       console.log('FizzBuzz');
-    } else if (index % 3) {
+    } else if (index % 3 === 0) {
       console.log('Fizz');
-    } else if (index % 5) {
+    } else if (index % 5 === 0) {
       console.log('Buzz');
     } else {
       console.log(index);
