70fdf295cc27483e5cbe03e6d1b9678efd808039
Julie Chung <juliehchung96@gmail.com>
2020-02-19 12:07:08 -0800

    Completed challenge, added test success

--- combined-consecutive-sequence/main.js
+++ combined-consecutive-sequence/main.js
@@ -8,14 +8,3 @@ function consecutiveCombo(a1, a2) {
   }
   return true;
 }
-
-const seq1 = [7, 4, 5, 1];
-const seq2 = [2, 3, 6];
-
-const misSeq1 = [1, 4, 6, 5];
-const misSeq2 = [2, 7, 8, 9];
-
-const awkNum1 = [44, 46];
-const awkNum2 = [45];
-
-console.log(consecutiveCombo(awkNum1, awkNum2));
