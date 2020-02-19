de9c074eb4c5d90dbf1c48b0c39bf2859b89f079
Julie Chung <juliehchung96@gmail.com>
2020-02-19 12:05:26 -0800

    Added test cases, fixed bug for overaccounting beyond array values

--- combined-consecutive-sequence/main.js
+++ combined-consecutive-sequence/main.js
@@ -1,7 +1,7 @@
 function consecutiveCombo(a1, a2) {
   const combinedArr = a1.concat(a2);
   combinedArr.sort((a, b) => a - b);
-  for (let index = 0; index < combinedArr.length; index++) {
+  for (let index = 0; index < combinedArr.length - 1; index++) {
     if (combinedArr[index] + 1 !== combinedArr[index + 1]) {
       return false;
     }
@@ -9,13 +9,13 @@ function consecutiveCombo(a1, a2) {
   return true;
 }
 
-// const seq1 = [7, 4, 5, 1];
-// const seq2 = [2, 3, 6];
+const seq1 = [7, 4, 5, 1];
+const seq2 = [2, 3, 6];
 
-// const misSeq1 = [1, 4, 6, 5];
-// const misSeq2 = [2, 7, 8, 9];
+const misSeq1 = [1, 4, 6, 5];
+const misSeq2 = [2, 7, 8, 9];
 
-// const awkNum1 = [44, 46];
-// const awkNum2 = [45];
+const awkNum1 = [44, 46];
+const awkNum2 = [45];
 
-// console.log(consecutiveCombo(awkNum1, awkNum2));
+console.log(consecutiveCombo(seq1, seq2));
