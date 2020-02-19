d27cf7db1c238b2523af9ccab5a110939d813368
Julie Chung <juliehchung96@gmail.com>
2020-02-19 12:04:35 -0800

    Completed first iteration of challenge

--- combined-consecutive-sequence/main.js
+++ combined-consecutive-sequence/main.js
@@ -1,3 +1,21 @@
 function consecutiveCombo(a1, a2) {
-
+  const combinedArr = a1.concat(a2);
+  combinedArr.sort((a, b) => a - b);
+  for (let index = 0; index < combinedArr.length; index++) {
+    if (combinedArr[index] + 1 !== combinedArr[index + 1]) {
+      return false;
+    }
+  }
+  return true;
 }
+
+// const seq1 = [7, 4, 5, 1];
+// const seq2 = [2, 3, 6];
+
+// const misSeq1 = [1, 4, 6, 5];
+// const misSeq2 = [2, 7, 8, 9];
+
+// const awkNum1 = [44, 46];
+// const awkNum2 = [45];
+
+// console.log(consecutiveCombo(awkNum1, awkNum2));
