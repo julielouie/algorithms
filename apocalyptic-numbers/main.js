aacdd4b4084f7ee842813fb6469784e40dc8de70
Julie Chung <juliehchung96@gmail.com>
2020-02-20 11:26:51 -0800

    Fixed index of days

--- apocalyptic-numbers/main.js
+++ apocalyptic-numbers/main.js
@@ -2,10 +2,8 @@ function apocalyptic(n) {
   const exp = Math.pow(2, n) + '';
   for (let index = 0; index < exp.length; index++) {
     if (exp[index] === '6' && exp[index + 1] === '6' && exp[index + 2] === '6') {
-      return `Repent! ${index} days until the Apocalypse!`;
+      return `Repent! ${index - 1} days until the Apocalypse!`;
     }
   }
   return 'Crisis averted. Resume sinning.';
 }
-
-console.log(apocalyptic(157));
