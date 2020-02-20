1ca4e780defb306729d9505a25bd92d05b14abf9
Julie Chung <juliehchung96@gmail.com>
2020-02-20 11:22:17 -0800

    Completed first step of challenge, starting testing

--- apocalyptic-numbers/main.js
+++ apocalyptic-numbers/main.js
@@ -1,3 +1,11 @@
 function apocalyptic(n) {
-
+  const exp = Math.pow(2, n) + '';
+  for (let index = 0; index < exp.length; index++) {
+    if (exp[index] === '6' && exp[index + 1] === '6' && exp[index + 2] === '6') {
+      return `Repent! ${index} days until the Apocalypse!`;
+    }
+  }
+  return 'Crisis averted. Resume sinning.';
 }
+
+console.log(apocalyptic(157));
