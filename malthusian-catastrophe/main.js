66d54626e02a7b406991e7015b955ccb4fd72885
Julie Chung <juliehchung96@gmail.com>
2020-02-24 15:25:07 -0800

    Tested successfully

--- malthusian-catastrophe/main.js
+++ malthusian-catastrophe/main.js
@@ -2,10 +2,12 @@ function malthusian(foodGrowth, popMult) {
   let year = 0;
   let foodProd = 100;
   let pop = 100;
-  while (foodProd <= pop) {
+  while (pop <= foodProd) {
     foodProd += foodGrowth;
     pop *= popMult;
     year++;
   }
   return year;
 }
+
+console.log(malthusian(3900, 1.26));
