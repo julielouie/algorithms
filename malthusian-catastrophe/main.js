51be4a78bf5ba6b32636fb4b7785617131eaa7f8
Julie Chung <juliehchung96@gmail.com>
2020-02-24 14:53:19 -0800

    Bug fixed returning incorrect value

--- malthusian-catastrophe/main.js
+++ malthusian-catastrophe/main.js
@@ -1,15 +1,11 @@
 function malthusian(foodGrowth, popMult) {
-  const catastrophe = {};
   let year = 0;
   let foodProd = 100;
   let pop = 100;
   while (foodProd <= pop) {
     foodProd += foodGrowth;
-    catastrophe.foodProd = foodProd;
     pop *= popMult;
-    catastrophe.pop = pop;
-    catastrophe.year = year;
     year++;
   }
-  return catastrophe;
+  return year;
 }
