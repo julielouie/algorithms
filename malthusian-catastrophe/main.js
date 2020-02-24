fd0ff67947cb8f88f873623bf8f8d07c99107e66
Julie Chung <juliehchung96@gmail.com>
2020-02-24 14:51:19 -0800

    Completed first iteration of challenge

--- malthusian-catastrophe/main.js
+++ malthusian-catastrophe/main.js
@@ -1,3 +1,15 @@
 function malthusian(foodGrowth, popMult) {
-
+  const catastrophe = {};
+  let year = 0;
+  let foodProd = 100;
+  let pop = 100;
+  while (foodProd <= pop) {
+    foodProd += foodGrowth;
+    catastrophe.foodProd = foodProd;
+    pop *= popMult;
+    catastrophe.pop = pop;
+    catastrophe.year = year;
+    year++;
+  }
+  return catastrophe;
 }
