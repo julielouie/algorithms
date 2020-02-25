3d211da73379b655098b940eca8732aecc24af04
Julie Chung <juliehchung96@gmail.com>
2020-02-25 14:07:43 -0800

    Completed testing

--- object-to-array/main.js
+++ object-to-array/main.js
@@ -9,3 +9,8 @@ function objectToArray(obj) {
   })
   return objArray;
 }
+
+const letters = { D: 1, B: 2, C: 3 };
+const words = { likes: 2, dislikes: 3, followers: 10 };
+
+console.log(objectToArray(words));
