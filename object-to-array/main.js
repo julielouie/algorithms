7263bfe93af5d3d500a6c0145bba10ad27a08289
Julie Chung <juliehchung96@gmail.com>
2020-02-25 14:03:26 -0800

    Completed first iteration of challenge

--- object-to-array/main.js
+++ object-to-array/main.js
@@ -1,3 +1,10 @@
 function objectToArray(obj) {
-
+  const objArray = [];
+  const keys = Object.keys(obj);
+  const values = Object.values(obj);
+  let valueIndex = 0;
+  keys.forEach(element => {
+    objArray.push([element, values[valueIndex]]);
+    valueIndex++;
+  })
 }
