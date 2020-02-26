9008eea2e38f798e1983cfe3723c16678d0308e6
Julie Chung <juliehchung96@gmail.com>
2020-02-26 14:16:43 -0800

    Bug fixing, issue understood

--- sock-merchant/main.js
+++ sock-merchant/main.js
@@ -1,35 +1,11 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-  inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-  inputString = inputString.replace(/\s*$/, '')
-    .split('\n')
-    .map(str => str.replace(/\s*$/, ''));
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
-// Complete the sockMerchant function below.
 function sockMerchant(n, ar) {
   let pairCount = 0;
+  debugger;
   for (let sockIndex = 0; sockIndex < n; sockIndex++) {
-    for (let secondSockInd = 0; secondSockInd < n; secondSockInd++) {
-      if (ar[sockIndex] === ar[secondSockInd]) {
+    for (let secondSockInd = 0; secondSockInd < ar.length; secondSockInd++) {
+      if (sockIndex === secondSockInd) {
+        continue;
+      } else if (ar[sockIndex] === ar[secondSockInd]) {
         pairCount++;
       }
     }
@@ -37,16 +13,7 @@ function sockMerchant(n, ar) {
   return pairCount;
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+const nEx = 9;
+const sockEx = [10, 20, 20, 10, 10, 30, 50, 10, 20];
 
-  const n = parseInt(readLine(), 10);
-
-  const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
-
-  let result = sockMerchant(n, ar);
-
-  ws.write(result + "\n");
-
-  ws.end();
-}
+console.log(sockMerchant(nEx, sockEx));
