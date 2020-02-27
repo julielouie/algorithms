456598b2d1c39714a067a4b898377ca1396a6bc3
Julie Chung <juliehchung96@gmail.com>
2020-02-27 20:46:32 +0000

    Added test case, consider 0 as sea level

--- counting-valleys/main.js
+++ counting-valleys/main.js
@@ -1,4 +1,20 @@
 function countingValleys(n, s) {
-
-
+  const stepCount = [];
+  for (let index = 0; index < s.length; index++) {
+    let character = s[index];
+    switch (character) {
+      case 'U':
+        stepCount.push(1);
+        break;
+      case 'D':
+        stepCount.push(-1);
+        break;
+    }
+  }
+  console.log(stepCount);
 }
+
+const nEx = 8;
+const strEx = 'UDDDUDUU';
+
+countingValleys(nEx, strEx);
