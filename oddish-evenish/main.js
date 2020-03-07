4bbcab8ed660e2614d7038434fd1be18b409617a
Julie Chung <juliehchung96@gmail.com>
2020-03-07 15:22:52 -0800

    Fixed bug added length of array to loop

--- oddish-evenish/main.js
+++ oddish-evenish/main.js
@@ -1,7 +1,7 @@
 function oddishOrEvenish(num) {
   const numArray = num.toString().split('');
   let currentNum = 0;
-  for (let index = 0; index < numArray; index++) {
+  for (let index = 0; index < numArray.length; index++) {
     currentNum += parseInt(numArray[index]);
   }
   if (currentNum % 2 === 1) {
