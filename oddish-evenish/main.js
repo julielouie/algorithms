59bc37067831d201af300e472f2618bbee10a0fd
Julie Chung <juliehchung96@gmail.com>
2020-03-07 15:20:23 -0800

    Completed first iteration of code challenge

--- oddish-evenish/main.js
+++ oddish-evenish/main.js
@@ -1,3 +1,12 @@
 function oddishOrEvenish(num) {
-
+  const numArray = num.toString().split('');
+  let currentNum = 0;
+  for (let index = 0; index < numArray; index++) {
+    currentNum += parseInt(numArray[index]);
+  }
+  if (currentNum % 2 === 1) {
+    return 'Oddish';
+  } else {
+    return 'Evenish';
+  }
 }
