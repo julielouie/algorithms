45675c3e14e4c47ff4079e2c98e8c268525826ee
Julie Chung <juliehchung96@gmail.com>
2020-03-09 22:16:52 +0000

    Challenge completed, added test success

--- 2D-array/main.js
+++ 2D-array/main.js
@@ -16,14 +16,3 @@ function hourglassSum(arr) {
   totals.sort((a, b) => a - b);
   return totals[totals.length - 1];
 }
-
-const array = [
-  [1, 1, 1, 0, 0, 0],
-  [0, 1, 0, 0, 0, 0],
-  [1, 1, 1, 0, 0, 0],
-  [0, 0, 2, 4, 4, 0],
-  [0, 0, 0, 2, 0, 0],
-  [0, 0, 1, 2, 4, 0]
-]
-
-console.log(hourglassSum(array));
