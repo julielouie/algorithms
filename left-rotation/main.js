d11543e62b2f3da131e5ea7e4a6e906c674b75d4
Julie Chung <juliehchung96@gmail.com>
2020-03-09 23:38:35 +0000

    Challenge completed, added test success

--- left-rotation/main.js
+++ left-rotation/main.js
@@ -3,8 +3,3 @@ function rotLeft(a, d) {
   const rotated = a.concat(removed);
   return rotated;
 }
-
-const array = [1, 2, 3, 4, 5];
-const rotations = 4;
-
-console.log(rotLeft(array, rotations));
