4641d343856a3f73b3b55efb1708349805496c98
Julie Chung <juliehchung96@gmail.com>
2020-03-09 23:34:56 +0000

    First iteration of code completed, starting testing

--- left-rotation/main.js
+++ left-rotation/main.js
@@ -1,3 +1,10 @@
 function rotLeft(a, d) {
-
+  const removed = a.splice(0, d);
+  a.concat(removed);
+  return a;
 }
+
+const array = [1, 2, 3, 4, 5];
+const rotations = 4;
+
+console.log(rotLeft(array, rotations));
