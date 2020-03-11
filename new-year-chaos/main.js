2be4ed5c735fd603a178d2d46030498669e473de
Julie Chung <juliehchung96@gmail.com>
2020-03-11 21:43:03 +0000

    Continuing challenge, implementing bubble sort algorithm

--- new-year-chaos/main.js
+++ new-year-chaos/main.js
@@ -1,3 +1,16 @@
-function minimumBribes(q) {
+function minimumbribeCount(q) {
+  let count = 0;
+  const bribeCount = {};
+  for (let index = 0; index < q.length; index++) {
+    for (let innerIndex = 0; innerIndex < q.length; innerIndex++) {
+      if (q[innerIndex] > q[innerIndex + 1]) {
+        bribeCount[q[innerIndex]] += bribeCount[q[innerIndex]];
+        let temp = q[innerIndex];
+        q[innerIndex] = q[innerIndex + 1];
+        q[innerIndex + 1] = temp;
+        count++;
+      }
+    }
+  }
 
 }
