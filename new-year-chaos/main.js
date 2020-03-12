ab80e20eeb3e6eaa340f3ac8e07e16b12effdc39
Julie Chung <juliehchung96@gmail.com>
2020-03-12 14:13:29 -0700

    Code refactoring complete, added test success

--- new-year-chaos/main.js
+++ new-year-chaos/main.js
@@ -15,35 +15,5 @@ function minimumbribeCount(q) {
       }
     }
   }
-
   return bribes;
-
-  // let chaos = false;
-  // let count = 0;
-  // const bribeCount = {};
-  // for (let index = 0; index < q.length; index++) {
-  //   if (q[index] > q[index + 1]) {
-  //     if (bribeCount[q[index]] === undefined) {
-  //       bribeCount[q[index]] = 0;
-  //     }
-  //     bribeCount[q[index]] += 1;
-  //     if (bribeCount[q[index]] > 2) {
-  //       chaos = true;
-  //     }
-  //     let temp = q[index];
-  //     q[index] = q[index + 1];
-  //     q[index + 1] = temp;
-  //     count++;
-  //     index = -1;
-  //   }
-  // }
-  // if (chaos) {
-  //   count = 'Too chaotic';
-  // }
-  // return count;
 }
-
-const queue = [2, 5, 1, 3, 4]
-const queue2 = [3, 2, 1, 4, 6, 5];
-
-console.log(minimumbribeCount(queue2));
