cc37b02f47fb8d64dd6a94fe2120700066f61d68
Julie Chung <juliehchung96@gmail.com>
2020-03-24 11:29:53 -0700

    Completed first iteration of code challenge

--- first-unrepeated-character/main.js
+++ first-unrepeated-character/main.js
@@ -1,3 +1,23 @@
 function firstNotRepeatingCharacter(s) {
-
+  const characters = {};
+  const length = s.length;
+  const count = 1;
+  for (let index = 0; index < length; index++) {
+    if (characters.hasOwnProperty(s[index])) {
+      characters[s[index]] += count;
+    } else {
+      characters[s[index]] = 1;
+      console.log(characters);
+    }
+  }
+  for (let key in characters) {
+    if (characters[key] === 1) {
+      return key;
+    }
+  }
+  return '_';
 }
+
+const string = "abacabad";
+
+console.log(firstNotRepeatingCharacter(string));
