a597fa74a1958cf0a60ac799bfcd666a01e0f9ec
Julie Chung <juliehchung96@gmail.com>
2020-03-23 20:39:33 -0700

    Completed first iteration of challenge

--- first-duplicate/main.js
+++ first-duplicate/main.js
@@ -1,17 +1,13 @@
 function firstDuplicate(a) {
-  let temp;
-  let tempIndex = [];
-  for (let outerIndex = 0; outerIndex < a.length; outerIndex++) {
-    temp = a[outerIndex];
-    a.forEach((element, index) => {
-      if (tempIndex[element] === undefined) {
-        if (temp === element && outerIndex !== index) {
-          tempIndex.push({ [temp]: [outerIndex, index] });
-        }
-      }
-    });
+  let indices = {};
+  for (let index = 0; index < a.length; index++) {
+    if (indices.hasOwnProperty([a[index]])) {
+      return a[index];
+    } else {
+      indices[a[index]] = a[index];
+    }
   }
-  return tempIndex
+  return -1;
 }
 
 const a = [2, 1, 3, 5, 3, 2];
