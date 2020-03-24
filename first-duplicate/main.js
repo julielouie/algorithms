706a282f0346056aeb22edca0d996d2df2f5c792
Julie Chung <juliehchung96@gmail.com>
2020-03-23 18:32:55 -0700

    Approaching challenged by using objects to keep track of indices

--- first-duplicate/main.js
+++ first-duplicate/main.js
@@ -1,3 +1,18 @@
 function firstDuplicate(a) {
-
+  let temp;
+  let tempIndex = [];
+  for (let outerIndex = 0; outerIndex < a.length; outerIndex++) {
+    temp = a[outerIndex];
+    tempIndex.push({[temp]: outerIndex});
+    a.forEach((element, index) => {
+      if (temp === element && outerIndex !== index) {
+        tempIndex.push({ [temp]: index });
+      }
+    });
+  }
+  return tempIndex
 }
+
+const a = [2, 1, 3, 5, 3, 2];
+
+console.log(firstDuplicate(a));
