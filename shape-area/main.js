6ff21eda165714d1af524ab5a7d910b921add4ba
Julie Chung <juliehchung96@gmail.com>
2020-03-26 15:36:59 -0700

    Refactored code to account for any number

--- shape-area/main.js
+++ shape-area/main.js
@@ -1,3 +1,20 @@
 function shapeArea(n) {
-  return (n * 4) + 1;
+  if (n === 1) {
+    return n;
+  }
+  let newN = n;
+  for (let index = 1; index < n; index++) {
+    newN += (4 * index) - 1;
+  }
+  return newN;
 }
+
+const n1 = 1;
+
+const n2 = 2;
+
+const n3 = 3;
+
+const n4 = 4;
+
+console.log(shapeArea(n4));
