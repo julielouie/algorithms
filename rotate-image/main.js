955d54cbc06ce8b3961e8ee3357c26aa611f1b04
Julie Chung <juliehchung96@gmail.com>
2020-03-27 13:21:57 -0700

    Working through visualization of array

--- rotate-image/main.js
+++ rotate-image/main.js
@@ -1,3 +1,14 @@
 function rotateImage(a) {
+  const newA = [];
+  for (let index = a.length - 1; index > -1; index--) {
 
+  }
 }
+
+const a = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+console.log(rotateImage(a));
