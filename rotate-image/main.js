d726a1454b74c0c74e8de8c516416b816e831cc5
Julie Chung <juliehchung96@gmail.com>
2020-03-28 15:06:49 -0700

    Modified to allow for multidigit numbers

--- rotate-image/main.js
+++ rotate-image/main.js
@@ -4,18 +4,22 @@ function rotateImage(a) {
   let count = 0;
   for (let index = a.length - 1; index > -1; index--) {
     let innerA = a[index][count];
-    imgStr += innerA;
-    if (innerA === a[0][innerLength]) break;
+    imgStr += innerA + ' ';
+    if (innerA === a[0][innerLength]) {
+      const lastSpace = imgStr.lastIndexOf(' ');
+      imgStr = imgStr.slice(0, lastSpace);
+      break;
+    }
     if (index === 0) {
-      imgStr += ' ';
+      imgStr += '--';
       count++;
       index = a.length;
     }
     if (count === innerLength + 1) count = 0;
   }
-  let newArray = imgStr.split(' ');
+  let newArray = imgStr.split(' --');
   newArray = newArray.map(element => {
-    return element.split('').map(Number);
+    return element.split(' ').map(Number);
   })
   return newArray;
 }
