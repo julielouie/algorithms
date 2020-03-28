e1497a1ccefb91a9a0182b744d3d7c52f3ca92af
Julie Chung <juliehchung96@gmail.com>
2020-03-28 14:10:47 -0700

    Accounting for groups of three

--- rotate-image/main.js
+++ rotate-image/main.js
@@ -1,11 +1,16 @@
 function rotateImage(a) {
   const newA = [];
+  let imgStr = '';
   let innerLength = a[0].length - 1;
   let count = 0;
   for (let index = a.length - 1; index > -1; index--) {
     let innerA = a[index][count];
+    count++;
+    imgStr += innerA;
+    if (imgStr.length % 3 === 0) imgStr += ' ';
     if (count === innerLength) count = 0;
-    if (a[index][count] === a[0][innerLength]) break;
+    if (innerA === a[0][innerLength]) break;
+    if (index === 0) index = a.length - 1;
   }
 }
 
