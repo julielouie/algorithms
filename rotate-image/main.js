453f0a3f5af5cd8fdc764362673b82749de44750
Julie Chung <juliehchung96@gmail.com>
2020-03-28 15:16:51 -0700

    Challenge completed, added test success

--- rotate-image/main.js
+++ rotate-image/main.js
@@ -23,13 +23,3 @@ function rotateImage(a) {
   })
   return newArray;
 }
-
-const a = [
-  [10, 9, 6, 3, 7],
-  [6, 10, 2, 9, 7],
-  [7, 6, 3, 8, 2],
-  [8, 9, 7, 9, 9],
-  [6, 8, 6, 8, 2]
-];
-
-console.log(rotateImage(a));
