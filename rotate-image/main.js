934a38c2211b6552b02339c7f8569feca292b2ac
Julie Chung <juliehchung96@gmail.com>
2020-03-28 14:00:30 -0700

    Accounting for multiple instances of loop

--- rotate-image/main.js
+++ rotate-image/main.js
@@ -1,10 +1,11 @@
 function rotateImage(a) {
   const newA = [];
-  let innerLength = a[0].length;
+  let innerLength = a[0].length - 1;
+  let count = 0;
   for (let index = a.length - 1; index > -1; index--) {
-    if (innerLength === 0) innerLength = a[0].length;
-
-
+    let innerA = a[index][count];
+    if (count === innerLength) count = 0;
+    if (a[index][count] === a[0][innerLength]) break;
   }
 }
 
