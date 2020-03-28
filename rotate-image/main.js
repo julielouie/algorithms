559b0d82194527b3d34b925281a2bebe5320af71
Julie Chung <juliehchung96@gmail.com>
2020-03-28 15:15:03 -0700

    Fixed bug for premature loop break

--- rotate-image/main.js
+++ rotate-image/main.js
@@ -5,7 +5,7 @@ function rotateImage(a) {
   for (let index = a.length - 1; index > -1; index--) {
     let innerA = a[index][count];
     imgStr += innerA + ' ';
-    if (innerA === a[0][innerLength]) {
+    if (index === 0 && count === innerLength && innerA === a[0][innerLength]) {
       const lastSpace = imgStr.lastIndexOf(' ');
       imgStr = imgStr.slice(0, lastSpace);
       break;
@@ -25,9 +25,11 @@ function rotateImage(a) {
 }
 
 const a = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
+  [10, 9, 6, 3, 7],
+  [6, 10, 2, 9, 7],
+  [7, 6, 3, 8, 2],
+  [8, 9, 7, 9, 9],
+  [6, 8, 6, 8, 2]
 ];
 
 console.log(rotateImage(a));
