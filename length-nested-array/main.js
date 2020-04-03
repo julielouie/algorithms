aca4d9979398e3849ceffa95b54f3de73e5160df
Julie Chung <juliehchung96@gmail.com>
2020-04-03 12:19:59 -0700

    Added getLength as array prototype method to use recursively

--- length-nested-array/main.js
+++ length-nested-array/main.js
@@ -1,4 +1,5 @@
 function getLength(arr) {
+  Array.prototype.getLength = function () {
   return this.reduce((totalLength, element) =>
     totalLength + (element.length ? element.getLength() : 1), 0);
   }
