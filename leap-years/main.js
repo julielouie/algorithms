b8cb23eb6a0f77ac184b8f2e011cb1f14022554c
Julie Chung <juliehchung96@gmail.com>
2020-04-04 13:37:12 -0700

    Added test cases

--- leap-years/main.js
+++ leap-years/main.js
@@ -1,3 +1,17 @@
 function isLeap(year) {
 
 }
+
+const y1 = 2020; //true
+// Exactly divided by 4 and not by 100.
+
+const y2 = 1800; //false
+// Exactly divided by 4, but is also exactly divided by 100.
+
+const y3 = 2000; //true
+// Exactly divided by 400.
+
+const y4 = 2019; //false
+// It can't be exactly divided by 400 or by 4.
+
+console.log(isLeap());
