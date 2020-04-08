c1b9b1ccabbfeb6e4c914c4c5ab557614bf9fce3
Julie Chung <juliehchung96@gmail.com>
2020-04-08 13:52:48 -0700

    Working through building internal arrays

--- picture-frame/main.js
+++ picture-frame/main.js
@@ -1,10 +1,36 @@
 function getFrame(w, h, ch) {
+  const frame = [];
+  let borderWidth = '';
+  let innerWidth = '';
+  let borderIndex = 0, innerIndex = 0;
 
+  while (borderIndex < w) {
+    borderWidth += `${ch}`;
+    borderIndex++;
+  }
+
+  while (innerIndex < w) {
+    if (innerWidth.length === w - 1) {
+      innerWidth += `${ch}`;
+      innerIndex++;
+    } else if (innerWidth[0] === `${ch}`) {
+      innerWidth += ' ';
+      innerIndex++;
+    } else {
+      innerWidth += `${ch}`;
+      innerIndex++;
+    }
+  }
+
+  const lastIndex = frame.length - 1;
+  if (!lastIndex) {
+    frame.push([ ])
+  }
 }
 
-//console.log(getFrame(4, 5, "#"));
+// console.log(getFrame(4, 5, "#"));
   // [
-  // ["####"],
+  //   ["####"],
   //   ["#  #"],
   //   ["#  #"],
   //   ["#  #"],
