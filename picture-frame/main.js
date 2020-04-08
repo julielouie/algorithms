f10f9c60d940e12003d0c3291b2b44433d6fcad2
Julie Chung <juliehchung96@gmail.com>
2020-04-08 13:35:48 -0700

    Updated test cases

--- picture-frame/main.js
+++ picture-frame/main.js
@@ -3,9 +3,23 @@ function getFrame(w, h, ch) {
 }
 
 //console.log(getFrame(4, 5, "#"));
+  // [
+  // ["####"],
+  //   ["#  #"],
+  //   ["#  #"],
+  //   ["#  #"],
+  //   ["####"]
+  // ]
   // Frame is 4 characters wide and 5 characters tall.
+
 //console.log(getFrame(10, 3, "*"));
+  // [
+  //   ["**********"],
+  //   ["*        *"],
+  //   ["**********"]
+  // ]
   // Frame is 10 characters and wide and 3 characters tall.
+
 //console.log(getFrame(2, 5, "0"));
   // "invalid"
   // Frame"s width is not more than 2.
