813383fb778a4df63c5526e7fc56b2bfa9cc3d49
Julie Chung <juliehchung96@gmail.com>
2020-04-09 14:02:11 -0700

    Completed first iteration of code

--- longest-common-prefix/main.js
+++ longest-common-prefix/main.js
@@ -1,9 +1,17 @@
-const longestCommonPrefix = function (strs) {
+const longestCommonPrefix = function (strArray) {
+  if (!strArray[0]) return '';
+
+  for (let prefixIndex = 0; prefixIndex < strArray[0].length; prefixIndex++) {
+    for (let string of strArray) {
+      if (string[prefixIndex] !== strArray[0][prefixIndex]) {
+        return string.slice(0, prefixIndex);
+      }
+    }
+  }
 
 };
 
-const strs1 = ["flower", "flow", "flight"];
-const strs2 = ["dog", "racecar", "car"];
+// const strs1 = ["flower", "flow", "flight"];
+// const strs2 = ["dog", "racecar", "car"];
 
 // console.log(longestCommonPrefix(strs1));
-// console.log(longestCommonPrefix(strs2));
