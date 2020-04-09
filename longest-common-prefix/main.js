fce206fb3d44375ae47e9d992ed3402b0ae7a4af
Julie Chung <juliehchung96@gmail.com>
2020-04-09 13:36:21 -0700

    Added test cases:

--- longest-common-prefix/main.js
+++ longest-common-prefix/main.js
@@ -1,3 +1,9 @@
 const longestCommonPrefix = function (strs) {
 
 };
+
+const strs1 = ["flower", "flow", "flight"];
+const strs2 = ["dog", "racecar", "car"];
+
+// console.log(longestCommonPrefix(strs1));
+// console.log(longestCommonPrefix(strs2));
