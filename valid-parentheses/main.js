cdcdde74b8b3d74b68d1eecf1efd8f8feb528ca8
Julie Chung <juliehchung96@gmail.com>
2020-04-10 12:50:26 -0700

    Added results

--- valid-parentheses/main.js
+++ valid-parentheses/main.js
@@ -4,7 +4,14 @@ const isValid = function (pString) {
 
 
 const case1 = "()";
+  // true
 const case2 = "()[]{}";
+  // true
 const case3 = "(]";
+  // false
 const case4 = "([)]";
+  // false
 const case5 = "{[]}";
+  // true
+
+// console.log(isValid(case1));
