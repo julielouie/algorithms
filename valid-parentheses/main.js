2477f088d165123844eea9c955df9b47b76f9456
Julie Chung <juliehchung96@gmail.com>
2020-04-10 14:43:31 -0700

    Passed all test cases successfully

--- valid-parentheses/main.js
+++ valid-parentheses/main.js
@@ -21,27 +21,3 @@ const isValid = function (pString) {
 
   return openingArray.length === 0 ? true : false;
 };
-
-
-const case1 = "()";
-  // true
-const case2 = "()[]{}";
-  // true
-const case3 = "(]";
-  // false
-const case4 = "([)]";
-  // false
-const case5 = "{[]}";
-  // true
-const case6 = "[])";
-  // false
-const case7 = "{}{}()[]";
-  // true
-const case8 = "{{)}";
-  // false
-const case9 = "(([]){})";
-  // true
-const case10 = "[({(())}[()])]";
-  // true
-
-console.log(isValid(case10));
