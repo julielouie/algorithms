c980cae8cc10cff58e6247222491b3c9725e87f9
Julie Chung <juliehchung96@gmail.com>
2020-04-10 13:20:16 -0700

    Completed first iteration of challenge

--- valid-parentheses/main.js
+++ valid-parentheses/main.js
@@ -1,5 +1,39 @@
 const isValid = function (pString) {
+  if (!pString) return true;
 
+  let valid = true;
+  let symbol = '';
+  let first = '';
+  let last = '';
+  let middle = 0;
+
+  while (valid) {
+    first = pString[0];
+
+    switch (first) {
+      case '(':
+        symbol = ')';
+        break;
+      case '[':
+        symbol = ']';
+        break;
+      case '{':
+        symbol = '}';
+        break;
+      default:
+        return false;
+    }
+
+    last = pString.lastIndexOf(symbol);
+    middle = last - 1;
+
+    if (!middle) return true;
+    if (middle % 2 !== 0) {
+      return false;
+    } else {
+      pString = pString.slice(1, last);
+    }
+  }
 };
 
 
@@ -14,4 +48,4 @@ const case4 = "([)]";
 const case5 = "{[]}";
   // true
 
-// console.log(isValid(case1));
+console.log(isValid(case5));
