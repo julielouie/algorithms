70b11b65613578b60bce06185acc81fd476d0461
Julie Chung <juliehchung96@gmail.com>
2020-04-11 11:46:28 -0700

    Added test case results

--- remove-duplicates-array/main.js
+++ remove-duplicates-array/main.js
@@ -3,6 +3,8 @@ const removeDuplicates = function (numsArray) {
 };
 
 const nums1 = [1, 1, 2];
+  // length = 2, with the first two elements of nums being 1 and 2 respectively
 const nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+  // length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4
 
 // console.log(removeDuplicates(nums1));
