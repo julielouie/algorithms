481225c5b6329fef770f55d4e16de567b45e9ee4
Julie Chung <juliehchung96@gmail.com>
2020-04-11 12:23:16 -0700

    Passed all test cases

--- remove-duplicates-array/main.js
+++ remove-duplicates-array/main.js
@@ -13,4 +13,4 @@ const nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 const nums3 = [0, 0, 0, 0, 0, 0, 0, 0];
   // length = 1, [0]
 
-console.log(removeDuplicates(nums2));
+console.log(removeDuplicates(nums1));
