d0f05f57a58641dfc06d8a8ee040d7a147c0ab5b
Julie Chung <juliehchung96@gmail.com>
2020-04-13 11:14:52 -0700

    Updated starter code for listnode

--- merge-two-sorted-lists/main.js
+++ merge-two-sorted-lists/main.js
@@ -1,13 +1,13 @@
-function ListNode(data, next) {
-  this.data = data;
-  this.next = next;
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
 }
 
 const mergeTwoLists = function (l1, l2) {
   let list = new ListNode();
   let head = list;
 
-  while (l1 !== null && l2 !== null) {
+  while (l1 !== null || l2 !== null) {
     if (l1.val < l2.val) {
       list.next = new ListNode(l1.val)
       l1 = l1.next
@@ -22,9 +22,3 @@ const mergeTwoLists = function (l1, l2) {
 
   return head.next;
 };
-
-const caseL1 = [1, 2, 4];
-const caseL2 = [1, 3, 4];
-  // 1->1->2->3->4->4
-
-// console.log(mergeTwoLists(caseL1, caseL2));
