f43cf2066245547e014bb49614e0d18a985efd1c
Julie Chung <juliehchung96@gmail.com>
2020-04-14 11:26:24 -0700

    Added test cases

--- remove-element/main.js
+++ remove-element/main.js
@@ -1,3 +1,11 @@
 const removeElement = function (nums, val) {
 
 };
+
+const nums1 = [3, 2, 2, 3];
+const val1 = 3;
+
+const nums2 = [0, 1, 2, 2, 3, 0, 4, 2];
+const val2 = 2;
+
+// console.log(removeElement(nums1, val1));
