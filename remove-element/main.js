ac7ba498052de26e7e78379da0ebd1c742c82e1e
Julie Chung <juliehchung96@gmail.com>
2020-04-14 11:49:09 -0700

    Passed all test cases

--- remove-element/main.js
+++ remove-element/main.js
@@ -7,11 +7,3 @@ const removeElement = function (nums, val) {
   }
   return nums.length;
 };
-
-const nums1 = [3, 2, 2, 3];
-const val1 = 3;
-
-const nums2 = [0, 1, 2, 2, 3, 0, 4, 2];
-const val2 = 2;
-
-console.log(removeElement(nums1, val1));
