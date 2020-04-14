c5886b29d3ce49ba755c995ccc36e92ed38e0976
Julie Chung <juliehchung96@gmail.com>
2020-04-14 12:22:37 -0700

    Completed first iteration of code

--- implement-strStr/main.js
+++ implement-strStr/main.js
@@ -1,5 +1,15 @@
 const strStr = function (haystack, needle) {
+  if (!needle) return 0;
 
+  const needleLen = needle.length;
+  for (let index = 0; index < haystack.length; index++) {
+    if (haystack[index] === needle[0]) {
+      let needleStr = haystack.slice(index, index + needleLen);
+      if (needle === needleStr) return index;
+    }
+  }
+
+  return -1;
 };
 
 const haystack1 = "hello";
@@ -8,4 +18,4 @@ const needle1 = "ll";
 const haystack2 = "aaaaa";
 const needle2 = "bba";
 
-// console.log(strStr(haystack1, needle1));
+console.log(strStr(haystack1, needle1));
