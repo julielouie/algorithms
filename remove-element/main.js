d70353bb4fbe2c61269d2d290c339f22bd375545
Julie Chung <juliehchung96@gmail.com>
2020-04-14 11:39:52 -0700

    Completed first iteration of code, only passes first test case

--- remove-element/main.js
+++ remove-element/main.js
@@ -1,5 +1,8 @@
 const removeElement = function (nums, val) {
-
+  nums.forEach((element, index) => {
+    if (element === val) nums.splice(index, 1);
+  })
+  return nums;
 };
 
 const nums1 = [3, 2, 2, 3];
@@ -8,4 +11,4 @@ const val1 = 3;
 const nums2 = [0, 1, 2, 2, 3, 0, 4, 2];
 const val2 = 2;
 
-// console.log(removeElement(nums1, val1));
+console.log(removeElement(nums2, val2));
