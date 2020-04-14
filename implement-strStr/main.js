ee1713df3823b72a8f9754d23acd8f8494ad7838
Julie Chung <juliehchung96@gmail.com>
2020-04-14 12:04:26 -0700

    Added basic test cases

--- implement-strStr/main.js
+++ implement-strStr/main.js
@@ -1,3 +1,11 @@
 const strStr = function (haystack, needle) {
 
 };
+
+const haystack1 = "hello";
+const needle1 = "ll";
+
+const haystack2 = "aaaaa";
+const needle2 = "bba";
+
+// console.log(strStr(haystack1, needle1));
