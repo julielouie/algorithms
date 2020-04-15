16c058859317ba8f2e44e08486a67b47346e7898
Julie Chung <juliehchung96@gmail.com>
2020-04-15 16:14:10 -0700

    Added basic test cases

--- longest-unrepeated-substring/main.js
+++ longest-unrepeated-substring/main.js
@@ -1,3 +1,12 @@
 const lengthOfLongestSubstring = function (string) {
 
 };
+
+const string1 = "abcabcbb";
+  // 3 "abc"
+const string2 = "bbbbb";
+  // 1 "b"
+const string3 = "pwwkew";
+  // 3 "wke"
+
+// console.log(lengthOfLongestSubstring(string1));
