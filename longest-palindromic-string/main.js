49ed53c46f9f33898a2e8fa328ed3b9565e97612
Julie Chung <juliehchung96@gmail.com>
2020-04-16 14:56:54 -0700

    Working through loop, considering removal of one for loop for efficiency

--- longest-palindromic-string/main.js
+++ longest-palindromic-string/main.js
@@ -1,5 +1,15 @@
 const longestPalindrome = function (string) {
-
+  const palindromes = [];
+  for (let index = 0; index < string.length; index++) {
+    let firstIndex = string[index];
+    for (let innerIndex = 0; innerIndex < string.length; innerIndex++) {
+      if (firstIndex === string[innerIndex] && index !== innerIndex) {
+        let section = string.slice(index, innerIndex);
+        let reversed = section.split('').reverse().join('');
+        if (section === reversed) palindromes.push(section);
+      }
+    }
+  }
 };
 
 const s1 = "babad";
