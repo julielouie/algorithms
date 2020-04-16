e5cf3e3d95a414081cea72c575abc60c4496213e
Julie Chung <juliehchung96@gmail.com>
2020-04-15 17:19:24 -0700

    Reformatted to use Set differently

--- longest-unrepeated-substring/main.js
+++ longest-unrepeated-substring/main.js
@@ -1,9 +1,22 @@
 const lengthOfLongestSubstring = function (string) {
-  const strSet = [...new Set(string)].join('');
-  if (!string.includes(strSet)) {
-    console.log(strSet)
+  const visited = new Set();
+  let left = 0;
+  let right = 0;
+  let resultLength = 0;
+
+  while (right < string.length) {
+    const currentChar = string.charAt(right);
+    if (!visited.has(currentChar)) {
+      visited.add(currentChar);
+      resultLength = Math.max(resultLength, right - left + 1);
+      right++;
+    } else {
+      visited.delete(string.charAt(left));
+      left++;
+    }
   }
-  return strSet.length;
+
+  return resultLength;
 };
 
 const string1 = "abcabcbb";
@@ -12,5 +25,6 @@ const string2 = "bbbbb";
   // 1 "b"
 const string3 = "pwwkew";
   // 3 "wke"
+const string4 = "wekwwp";
 
-console.log(lengthOfLongestSubstring(string3));
+console.log(lengthOfLongestSubstring(string1));
