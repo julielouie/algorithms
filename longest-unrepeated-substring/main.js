f1b50c88ffd5c76cca58e5fce43cd8537925de85
Julie Chung <juliehchung96@gmail.com>
2020-04-15 17:27:06 -0700

    Challenge completed, added test success

--- longest-unrepeated-substring/main.js
+++ longest-unrepeated-substring/main.js
@@ -18,13 +18,3 @@ const lengthOfLongestSubstring = function (string) {
 
   return resultLength;
 };
-
-const string1 = "abcabcbb";
-  // 3 "abc"
-const string2 = "bbbbb";
-  // 1 "b"
-const string3 = "pwwkew";
-  // 3 "wke"
-const string4 = "wekwwp";
-
-console.log(lengthOfLongestSubstring(string1));
