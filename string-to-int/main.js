998622382c14451c6a63c019e37432b978d111fe
Julie Chung <juliehchung96@gmail.com>
2020-04-17 12:52:42 -0700

    Completed functionality, need to add condition for 32 bit signed integer

--- string-to-int/main.js
+++ string-to-int/main.js
@@ -1,3 +1,24 @@
 const myAtoi = function (string) {
-
+  let trimmed = string.trim();
+  let newStr = '';
+  for (let index = 0; index < trimmed.length; index++) {
+    let currentIndexNum = +(trimmed[index]);
+    if (typeof currentIndexNum === 'number' && !Number.isNaN(currentIndexNum) && trimmed[index] !== ' ') {
+      newStr += trimmed[index];
+    } else if (trimmed[index] === '-' && (typeof +(trimmed[index + 1])) === 'number') {
+      newStr += '-';
+    } else {
+      continue;
+    }
+  }
+  newStr = parseInt(newStr);
+  console.log('number: ', newStr)
 };
+
+const s1 = "42";
+const s2 = "      -42";
+const s3 = "4193 with words";
+const s4 = "words and 987";
+const s5 = "-91283472332";
+
+console.log(myAtoi(s5));
