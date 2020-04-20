00c22d0193e5ce9f86e977ef539d505a207984b3
Julie Chung <juliehchung96@gmail.com>
2020-04-20 10:40:15 -0700

    Completed first iteration of code, passed basic test cases

--- search-insert-position/main.js
+++ search-insert-position/main.js
@@ -1,5 +1,14 @@
 const searchInsert = function (nums, target) {
-
+  //debugger;
+  // const numsSet = new Set(nums);
+  // const hasTarget = numsSet.has(target);
+  for (let index = 0; index < nums.length; index++) {
+    let current = nums[index];
+    if (current === target) return index;
+    else if (current > target) return index;
+    else if (index === nums.length - 1 && target > current) return index + 1;
+  }
+  return 0;
 };
 
 const nums1 = [1, 3, 5, 6];
@@ -17,3 +26,5 @@ const target3 = 7;
 const nums4 = [1, 3, 5, 6];
 const target4 = 0;
   // 0
+
+console.log(searchInsert(nums4, target4))
