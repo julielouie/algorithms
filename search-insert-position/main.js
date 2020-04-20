6028007c0df3c68b693545e76df1ab7b4c9da22e
Julie Chung <juliehchung96@gmail.com>
2020-04-20 10:41:42 -0700

    Removed test environment

--- search-insert-position/main.js
+++ search-insert-position/main.js
@@ -7,21 +7,3 @@ const searchInsert = function (nums, target) {
   }
   return 0;
 };
-
-const nums1 = [1, 3, 5, 6];
-const target1 = 5;
-  // 2
-
-const nums2 = [1, 3, 5, 6];
-const target2 = 2;
-  // 1
-
-const nums3 = [1, 3, 5, 6];
-const target3 = 7;
-  // 4
-
-const nums4 = [1, 3, 5, 6];
-const target4 = 0;
-  // 0
-
-console.log(searchInsert(nums4, target4))
