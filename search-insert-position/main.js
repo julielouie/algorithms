c6a45f03e1f2fe64fd275039b87a686bf5045191
Julie Chung <juliehchung96@gmail.com>
2020-04-20 10:44:18 -0700

    Challenge completed, added test success

--- search-insert-position/main.js
+++ search-insert-position/main.js
@@ -1,8 +1,7 @@
 const searchInsert = function (nums, target) {
   for (let index = 0; index < nums.length; index++) {
     let current = nums[index];
-    if (current === target) return index;
-    else if (current > target) return index;
+    if (current === target || current > target) return index;
     else if (index === nums.length - 1 && target > current) return index + 1;
   }
   return 0;
