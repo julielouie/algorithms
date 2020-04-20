919001e601f3ea20505e900334688b101d742558
Julie Chung <juliehchung96@gmail.com>
2020-04-20 10:22:31 -0700

    Added basic test cases

--- search-insert-position/main.js
+++ search-insert-position/main.js
@@ -1,3 +1,15 @@
 const searchInsert = function (nums, target) {
 
 };
+
+const nums1 = [1, 3, 5, 6];
+const target1 = 5;
+
+const nums2 = [1, 3, 5, 6];
+const target2 = 2;
+
+const nums3 = [1, 3, 5, 6];
+const target3 = 7;
+
+const nums4 = [1, 3, 5, 6];
+const target4 = 0;
