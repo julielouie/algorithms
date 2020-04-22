39005ddfb9bf830a80f14d3c2fd06e6970016548
Julie Chung <juliehchung96@gmail.com>
2020-04-22 13:19:54 -0700

    Refactored code to accomodate extremely large binary numbers

--- add-binary/main.js
+++ add-binary/main.js
@@ -1,9 +1,8 @@
 const addBinary = function (bin1, bin2) {
-  const num1 = parseInt(bin1, 2);
-  const num2 = parseInt(bin2, 2);
-  let result = num1 + num2;
-  result = (result >>> 0).toString(2);
-  return result;
+  const num1 = `0b${bin1}`;
+  const num2 = `0b${bin2}`;
+  const result = BigInt(num1) + BigInt(num2);
+  return result.toString(2);
 };
 
 const in1 = '11';
@@ -12,4 +11,7 @@ const in2 = '1';
 const in3 = '1010';
 const in4 = '1011';
 
-console.log(addBinary(in3, in4));
+const in5 = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+const in6 = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+
+console.log(addBinary(in5, in6));
