613936aa26d80d09f33622fa1b9f6ded5927ee23
Julie Chung <juliehchung96@gmail.com>
2020-04-22 13:11:59 -0700

    Passed basic test cases

--- add-binary/main.js
+++ add-binary/main.js
@@ -1,5 +1,9 @@
 const addBinary = function (bin1, bin2) {
-
+  const num1 = parseInt(bin1, 2);
+  const num2 = parseInt(bin2, 2);
+  let result = num1 + num2;
+  result = (result >>> 0).toString(2);
+  return result;
 };
 
 const in1 = '11';
@@ -8,4 +12,4 @@ const in2 = '1';
 const in3 = '1010';
 const in4 = '1011';
 
-console.log(addBinary(in1, in2));
+console.log(addBinary(in3, in4));
