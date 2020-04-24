de43a21dd4fa222ad889db5e6e27118c9b151c66
Julie Chung <juliehchung96@gmail.com>
2020-04-24 13:12:00 -0700

    Starter code and basic test cases added

---
+++ is-crypt-solution/main.js
@@ -0,0 +1,26 @@
+function isCryptSolution(crypt, solution) {
+
+}
+
+const crypt1 = ["SEND", "MORE", "MONEY"];
+const solution1 = [
+  ['O', '0'],
+  ['M', '1'],
+  ['Y', '2'],
+  ['E', '5'],
+  ['N', '6'],
+  ['D', '7'],
+  ['R', '8'],
+  ['S', '9']
+];
+
+const crypt2 = ["TEN", "TWO", "ONE"];
+const solution2 = [
+  ['O', '1'],
+  ['T', '0'],
+  ['W', '9'],
+  ['E', '5'],
+  ['N', '4']
+];
+
+// console.log(isCryptSolution(crypt1, solution1));
