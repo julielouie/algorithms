df09f18812a4a34c842a7f9f847f064a9d67fa91
Julie Chung <juliehchung96@gmail.com>
2020-04-23 17:43:40 -0700

    Implementing multiple internal functions to break down steps

--- sudoku/main.js
+++ sudoku/main.js
@@ -1,12 +1,20 @@
 function sudoku2(grid) {
-  for (let outermost = 0; outermost < grid.length; outermost++) {
-    const current = grid[outermost];
-    const noDuplicates = [...new Set(current)];
+  for (let outer = 0; outer < grid.length; outer++) {
+    const currentHorizontal = grid[outer];
+    if (checkArray(currentHorizontal)) {
+      continue;
+    } else {
+      return false;
+    }
+  }
 
-    const currentArray = addArray(current);
-    const currentSet = addArray(noDuplicates);
-    if (currentArray !== currentSet) return false;
-
+  for (let outerVert = 0; outerVert < grid.length; outerVert++) {
+    let currentVertical = grid[outerVert];
+    if (checkArray(currentVertical)) {
+      continue;
+    } else {
+      return false;
+    }
   }
 
   const addArray = (arrayToAdd) => {
@@ -16,6 +24,29 @@ function sudoku2(grid) {
       }
     }, 0);
     return added;
+  }
+
+  const checkArray = (arrayToCheck) => {
+    const noDuplicates = [...new Set(arrayToCheck)];
+
+    const currentArray = addArray(arrayToCheck);
+    const currentSet = addArray(noDuplicates);
+    if (currentArray !== currentSet) return false;
+    else return true;
+  }
+
+  const calculateVertical = (arrayToCalc) => {
+    let count = 9;
+    let vertIndex = 0;
+    let vertical = [];
+
+    while (count > 0) {
+      vertical.push(grid[arrayToCalc][vertIndex]);
+      vertIndex++;
+      count--;
+    }
+
+    return checkArray(vertical);
   }
 }
 
@@ -43,4 +74,4 @@ const grid2 = [
   ['.', '2', '.', '.', '3', '.', '.', '.', '.']
   ];
 
-console.log(sudoku2(grid1));
+// console.log(sudoku2(grid1));
