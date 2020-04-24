da5eb72d51f45c761b2af0a04adb91f5e3bbe2d7
Julie Chung <juliehchung96@gmail.com>
2020-04-24 13:26:16 -0700

    Plan laid out for solution

--- is-crypt-solution/main.js
+++ is-crypt-solution/main.js
@@ -1,4 +1,17 @@
 function isCryptSolution(crypt, solution) {
+  //convert solution array into object
+  const solutionsObj = {};
+  solution.forEach(element => solutionsObj[element[0]] = element[1]);
+
+
+  //index through each string in crypt
+  //in each string, index through each character
+  //check if solution object has this character as a property
+  //replace if yes, return false if not
+  //check for leading 0's once all characters have been replaced
+  //change strings to numbers
+  //do the math
+  //return true or false
 
 }
 
@@ -23,4 +36,4 @@ const solution2 = [
   ['N', '4']
 ];
 
-// console.log(isCryptSolution(crypt1, solution1));
+console.log(isCryptSolution(crypt1, solution1));
