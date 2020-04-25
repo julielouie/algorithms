5a3e79f92ec2fba9dfb5b1484d456f6e83e11b2b
Julie Chung <juliehchung96@gmail.com>
2020-04-25 13:11:54 -0700

    Added starter code and basic test cases

---
+++ zigzag-conversion/main.js
@@ -0,0 +1,11 @@
+const convert = function (s, numRows) {
+
+};
+
+const s1 = "PAYPALISHIRING";
+const numRows1 = 3;
+
+const s2 = "PAYPALISHIRING";
+const numRows2 = 4;
+
+console.log(convert(s1, numRows1));
