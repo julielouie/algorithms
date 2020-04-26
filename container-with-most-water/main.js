0ab3b0cbc47aee74034b944734dba5b0f71fad4e
Julie Chung <juliehchung96@gmail.com>
2020-04-26 09:53:54 -0700

    Made basic plan

--- container-with-most-water/main.js
+++ container-with-most-water/main.js
@@ -6,3 +6,14 @@ const test1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
   // output: 49
 
 // console.log(maxArea(test1));
+
+
+
+/* - if height is empty, return 0
+   - sort array of given heights
+   - loop through to calculate area of each index w/resp to one index
+   - in each iteration, calculate a = l x w
+   - push each value to an array
+   - sort this array
+   - return 0 if empty, else return largest value
+*/
