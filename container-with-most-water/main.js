1cd1166eeb6b3f956c412d6336e2347b9afb433f
Julie Chung <juliehchung96@gmail.com>
2020-04-26 10:52:42 -0700

    Passed all test cases

--- container-with-most-water/main.js
+++ container-with-most-water/main.js
@@ -32,18 +32,3 @@ const maxArea = function (height) {
     areas.push(temp);
   }
 };
-
-const test1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
-  // output: 49
-
-console.log(maxArea(test1));
-
-
-
-/* - if height is empty, return 0
-   - loop through to calculate area of each index w/resp to one index
-   - in each iteration, calculate a = l x w
-   - push each value to an array
-   - sort this array
-   - return 0 if empty, else return largest value
-*/
