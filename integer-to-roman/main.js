eebb7cd6381bc2fe08ac5d8241422277d9f3cad1
Julie Chung <juliehchung96@gmail.com>
2020-04-27 13:32:02 -0700

    Passed all test cases

--- integer-to-roman/main.js
+++ integer-to-roman/main.js
@@ -6,11 +6,3 @@ const intToRoman = function (num) {
     return result;
   }, '');
 };
-
-const num1 = 3;
-const num2 = 4;
-const num3 = 9;
-const num4 = 58;
-const num5 = 1996;
-
-console.log(intToRoman(num5));
