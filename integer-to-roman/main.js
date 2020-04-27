9414346653a7c953edbc61783a7d0ef7b5c51c99
Julie Chung <juliehchung96@gmail.com>
2020-04-27 13:29:25 -0700

    Added 9 and 4 conditions

--- integer-to-roman/main.js
+++ integer-to-roman/main.js
@@ -1,5 +1,5 @@
 const intToRoman = function (num) {
-  const map = { M: 1000, D: 900, C: 100, L: 50, X: 10, V: 5, I: 1 };
+  const map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
   return Object.entries(map).reduce((result, [romanInt, number]) => {
     result = romanInt.repeat(Math.floor(num / number));
     num %= number;
