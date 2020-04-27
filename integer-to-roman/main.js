c7944293df063dd3be9d0372f2e09d0493a001d8
Julie Chung <juliehchung96@gmail.com>
2020-04-27 13:13:49 -0700

    Completed basic function

--- integer-to-roman/main.js
+++ integer-to-roman/main.js
@@ -1,9 +1,11 @@
 const intToRoman = function (num) {
   const map = { M: 1000, D: 900, C: 100, L: 50, X: 10, V: 5, I: 1 };
-  let result = '';
-  Object.entries(map).reduce((result, [romanInt, number]) => {
+  return Object.entries(map).reduce((result, [romanInt, number]) => {
+    result = romanInt.repeat(Math.floor(num / number));
+    num %= number;
+    return result;
+  }, '');
 
-  })
 };
 
 const num1 = 3;
@@ -12,4 +14,4 @@ const num3 = 9;
 const num4 = 58;
 const num5 = 1996;
 
-// console.log(intToRoman(num1));
+console.log(intToRoman(num2));
