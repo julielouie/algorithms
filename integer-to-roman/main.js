5051531dd1d0e7d00595e06a9dffcce60d636959
Julie Chung <juliehchung96@gmail.com>
2020-04-27 12:57:15 -0700

    Added test cases and starter code

---
+++ integer-to-roman/main.js
@@ -0,0 +1,13 @@
+const intToRoman = function (num) {
+  const map = { M: 1000, D: 900, C: 100, L: 50, X: 10, V: 5, I: 1 };
+
+
+};
+
+const num1 = 3;
+const num2 = 4;
+const num3 = 9;
+const num4 = 58;
+const num5 = 1996;
+
+// console.log(intToRoman(num1));
