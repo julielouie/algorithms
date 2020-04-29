64b49f590c4d3298470a22920a4a3727316704fa
Julie Chung <juliehchung96@gmail.com>
2020-04-28 18:58:48 -0700

    Added error handling

--- taxes-in-cents/main.js
+++ taxes-in-cents/main.js
@@ -1,4 +1,12 @@
 function calcTaxCents(dollars, percent) {
+  if (!dollars || !percent || Number.isNaN(parseInt(dollars)) || Number.isNaN(parseInt(percent))) {
+    return 'Please provide valid monetary amounts!'
+  }
+  if (typeof dollars !== 'number' || typeof percent !== 'number') {
+    dollars = +dollars;
+    percent = +percent;
+  }
+
 
 }
 
@@ -8,4 +16,5 @@ const percent1 = 20;
 const dollars2 = 720.46;
 const percent2 = 15;
 
+console.log(calcTaxCents('900', '20'))
 // console.log(calcTaxCents(dollars1, percent1));
