1e5c1b83ce0c31ae4ef31ddc02043382d1660138
Julie Chung <juliehchung96@gmail.com>
2020-04-28 18:46:48 -0700

    Added basic test cases

---
+++ taxes-in-cents/main.js
@@ -0,0 +1,11 @@
+function calcTaxCents(dollars, percent) {
+
+}
+
+const dollars1 = 900.50;
+const percent1 = 20;
+
+const dollars2 = 720.46;
+const percent2 = 15;
+
+// console.log(calcTaxCents(dollars1, percent1));
