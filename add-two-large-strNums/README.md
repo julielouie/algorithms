abce7ee51e615c3d5d5e2dcf720ba252b5f403b3
Julie Chung <juliehchung96@gmail.com>
2020-04-30 11:30:18 -0700

    Adding coding challenge provided last night by hiring manager

---
+++ add-two-large-strNums/README.md
@@ -0,0 +1,19 @@
+# Add Two Large Numbers (Strings)
+
+Coding challenge provided during interview for Frontend Engineer position:
+
+Please create a function that takes in two strings.
+Assume that each string is a very large number.
+Add the two together and return the sum.
+
+### Example 1:
+```shell
+Input: word1 = "silent", word2 = "listen"
+Output: true
+```
+
+### Example 2:
+```shell
+Input: word1 = "yes", word2 = "no"
+Output: false
+```
---
+++ add-two-large-strNums/coding-challenge.js
@@ -0,0 +1,41 @@
+function convToInt(largeNum1, largeNum2) {
+  return (BigInt(largeNum1) + BigInt(largeNum2)).toString();
+}
+
+function addTwo(strNum1, strNum2) {
+  let result = "";
+  let length1 = strNum1.length;
+  let length2 = strNum2.length;
+
+  if (length2 > length1) {
+    let tempVal = strNum2;
+    strNum2 = strNum1;
+    strNum1 = tempVal;
+  }
+
+  let move = 0;
+  let first;
+  let second;
+  let placeHolder;
+  let numSum;
+
+  for (let index = 0; index < strNum1.length; index++) {
+    first = parseInt(strNum1.charAt(strNum1.length - 1 - index));
+    second = parseInt(strNum2.charAt(strNum2.length - 1 - index));
+    second = (second) ? second : 0;
+
+    placeHolder = (move + first + second).toString();
+    numSum = placeHolder.charAt(placeHolder.length - 1);
+
+    move = parseInt(placeHolder.substr(0, placeHolder.length - 1));
+    move = (move) ? move : 0;
+
+    result = (index === strNum1.length - 1) ? placeHolder + result : numSum + result;
+  }
+
+  return result;
+}
+
+console.log('firstFunction: ', convToInt('9826581412335', '98789878998'));
+
+console.log('secondFunction: ', addTwo('9826581412335', '98789878998'));
