ad89801ae1e7c152926e0ddcee4cf9b971474984
Julie Chung <juliehchung96@gmail.com>
2020-04-30 13:50:57 -0700

    Completed basic functionality

--- quarantine-credit-card/main.js
+++ quarantine-credit-card/main.js
@@ -1,9 +1,16 @@
 function secure(cardNums) {
   let result = '';
 
+  const numsCheck = /^[0-9]*$/g;
   cardNums = cardNums.replace(/-|\s/g, '');
-  const numsOnly = (/^[0-9]*$/g).test(cardNums);
-  if (!numsOnly) return false;
+  if (!(numsCheck).test(cardNums)) return false;
+
+  let maskLength = cardNums.length - 4;
+  const last4 = cardNums.slice(maskLength);
+  for (let index = 0; index < maskLength; index++) {
+    result += cardNums.replace(numsCheck, '*');
+  }
+  return result += last4;
 }
 
 const card1 = '1234567891234567';
@@ -18,4 +25,4 @@ const card3 = '9780-2381-4927-4590';
 const invalid = '1234 sdfh 4848 wert';
  // Output: ************4590
 
-console.log(secure(invalid));
+console.log(secure(card1));
