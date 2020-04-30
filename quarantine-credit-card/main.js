871a26a98e71bcd8d0f1f954f03441aba405e1fa
Julie Chung <juliehchung96@gmail.com>
2020-04-30 13:27:27 -0700

    Using regex to only take card numbers

--- quarantine-credit-card/main.js
+++ quarantine-credit-card/main.js
@@ -1,5 +1,5 @@
 function secure(cardNums) {
-
+  const numsRegEx = RegExp(/^[0-9]*$/g);
 }
 
 const card1 = '1234567891234567';
@@ -9,4 +9,7 @@ const card2 = '9857 4565 1354 8916';
   // Output: ************8916
 
 const card3 = '9780-2381-4927-4590';
+ // Output: ************4590
+
+const invalid = '1234 sdfh 4848 wert';
  // Output: ************4590
