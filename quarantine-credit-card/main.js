a8f0054f14aabed1e5ab4fdd3ce13ac0e519c97e
Julie Chung <juliehchung96@gmail.com>
2020-04-30 12:34:24 -0700

    Adding starting code and basic test cases

---
+++ quarantine-credit-card/main.js
@@ -0,0 +1,12 @@
+function secure(cardNums) {
+
+}
+
+const card1 = '1234567891234567';
+ // Output: ************4567
+
+const card2 = '9857 4565 1354 8916';
+  // Output: ************8916
+
+const card3 = '9780-2381-4927-4590';
+ // Output: ************4590
