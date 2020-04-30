154b1c9117de11c5e721d7c236470c348e772b7b
Julie Chung <juliehchung96@gmail.com>
2020-04-30 13:54:04 -0700

    Challenge completed

--- quarantine-credit-card/main.js
+++ quarantine-credit-card/main.js
@@ -3,7 +3,7 @@ function secure(cardNums) {
 
   const numsCheck = /^[0-9]*$/g;
   cardNums = cardNums.replace(/-|\s/g, '');
-  if (!(numsCheck).test(cardNums)) return false;
+  if (!(numsCheck).test(cardNums)) return 'Please enter a valid card number';
 
   let maskLength = cardNums.length - 4;
   const last4 = cardNums.slice(maskLength);
@@ -12,17 +12,3 @@ function secure(cardNums) {
   }
   return result += last4;
 }
-
-const card1 = '1234567891234567';
- // Output: ************4567
-
-const card2 = '9857 4565 1354 8916';
-  // Output: ************8916
-
-const card3 = '9780-2381-4927-4590';
- // Output: ************4590
-
-const invalid = '1234 sdfh 4848 wert';
- // Output: ************4590
-
-console.log(secure(card1));
