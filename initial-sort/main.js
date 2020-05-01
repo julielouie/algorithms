3014c007f6c2d050a1ebd13abb9127198406a2c7
Julie Chung <juliehchung96@gmail.com>
2020-05-01 12:05:16 -0700

    Completed basic functionality

--- initial-sort/main.js
+++ initial-sort/main.js
@@ -1,11 +1,31 @@
 function sortInitials(initials) {
-
+  initials = initials.toLowerCase();
+  const map = {};
+  let newName = '';
+  for (let index = 0; index < initials.length; index++) {
+    if (!map.hasOwnProperty(initials[index])) {
+      map[initials[index]] = initials.charCodeAt(index);
+    }
+  }
+  while (Object.keys(map).length !== 0) {
+    let temp = Object.keys(map)[0];
+    for (let key in map) {
+      if (map[key] < map[temp]) {
+        temp = key;
+      }
+    }
+    newName += temp.toUpperCase();
+    delete map[temp];
+  }
+  return newName;
 }
 
-const initials1 = 'IBHSDJ';
+const initials1 = 'ibhsdj';
  //Output: 'BDHIJS'
 
-const initials2 = 'POSH';
+const initials2 = 'posh';
  //Output: 'HOPS'
 
-console.log(sortInitials(initials1));
+
+
+console.log(sortInitials(initials2));
