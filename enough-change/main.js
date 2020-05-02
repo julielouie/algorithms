93a40a82d9872691e459dfc580e3841c47962260
Julie Chung <juliehchung96@gmail.com>
2020-05-02 14:08:46 -0700

    Passes basic test cases

--- enough-change/main.js
+++ enough-change/main.js
@@ -1,12 +1,14 @@
 function calcChange(total, change) {
-  console.log(change)
   change[0] *= 0.01;
   change[1] *= 0.05;
   change[2] *= 0.10;
   change[3] *= 0.25;
-  const totalChange = change.reduce((acc, curr) => {
+  let totalChange = change.reduce((acc, curr) => {
     return acc + (+curr);
-  }, 0)
+  }, 0);
+  totalChange = parseFloat(totalChange.toFixed(2));
+  if (total <= totalChange) return true;
+  return false;
 }
 
 const total1 = 34.51;
