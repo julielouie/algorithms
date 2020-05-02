3f4c4f109757d950337efbf3e3288b0d772ad4cf
Julie Chung <juliehchung96@gmail.com>
2020-05-02 14:12:42 -0700

    Completed challenge

--- enough-change/main.js
+++ enough-change/main.js
@@ -10,15 +10,3 @@ function calcChange(total, change) {
   if (total <= totalChange) return true;
   return false;
 }
-
-const total1 = 34.51;
-const change1 = [1, 2, 3, 4];
-  // Output: false
-
-
-
-const total2 = 2.45;
-const change2 = [1, 5, 2, 10];
-  // Output: true
-
-console.log(calcChange(total1, change1));
