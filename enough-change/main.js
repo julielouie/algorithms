0b6ff7a48660a62998115d3437c013f1a7acc870
Julie Chung <juliehchung96@gmail.com>
2020-05-02 13:48:48 -0700

    Added basic test cases

---
+++ enough-change/main.js
@@ -0,0 +1,15 @@
+function calcChange(total, change) {
+
+}
+
+const total1 = 34.51;
+const change1 = [1, 2, 3, 4];
+  // Output: false
+
+
+
+const total2 = 2.45;
+const change2 = [1, 5, 2, 10];
+  // Output: true
+
+console.log(calcChange(total1, change1));
