562b026c0fe18175335c2919565510002d9cf1ff
Julie Chung <juliehchung96@gmail.com>
2020-05-03 10:23:49 -0700

    Added basic test cases

--- math-interview/main.js
+++ math-interview/main.js
@@ -1,3 +1,28 @@
 function calcNums(num1, num2, op) {
 
 }
+
+const num1 = 2;
+const num2 = 8;
+const op = '+';
+ // Output: 10
+
+// const num1 = 20;
+// const num2 = 5;
+// const op = '-';
+//  // Output: 15
+
+// const num1 = 2;
+// const num2 = 14;
+// const op = '*';
+//  // Output: 28
+
+// const num1 = 36;
+// const num2 = 6;
+// const op = '/';
+//  // Output: 6
+
+// const num1 = 5;
+// const num2 = 0;
+// const op = '/';
+ // Output: 'Cannot divide a number by 0!'
