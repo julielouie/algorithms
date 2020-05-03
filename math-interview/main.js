6e3c09927c2b77b7ae5e486adb86a18c9053b1fd
Julie Chung <juliehchung96@gmail.com>
2020-05-03 10:30:45 -0700

    Challenge completed

--- math-interview/main.js
+++ math-interview/main.js
@@ -11,37 +11,4 @@ function calcNums(num1, num2, op) {
       if (num2 === 0) return 'Cannot divide a number by 0!';
       return num1 / num2;
   }
-
 }
-
-// const num1 = 2;
-// const num2 = 8;
-// const op = '+';
-//  // Output: 10
-
-// const num1 = 20;
-// const num2 = 5;
-// const op = '-';
-//  // Output: 15
-
-// const num1 = 2;
-// const num2 = 14;
-// const op = '*';
-//  // Output: 28
-
-// const num1 = 2;
-// const num2 = 14;
-// const op = 'x';
-//  // Output: 28
-
-// const num1 = 36;
-// const num2 = 6;
-// const op = '/';
-//  // Output: 6
-
-// const num1 = 5;
-// const num2 = 0;
-// const op = '/';
-//  'Cannot divide a number by 0!'
-
-console.log(calcNums(num1, num2, op));
