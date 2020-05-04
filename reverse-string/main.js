f8f0b1602f782ef60f635240f1d680578ee0fee5
Julie Chung <juliehchung96@gmail.com>
2020-05-04 11:26:47 -0700

    Completed funtionality

--- reverse-string/main.js
+++ reverse-string/main.js
@@ -1,5 +1,7 @@
 function reverseString(str) {
-
+  let reversed = '';
+  (str.split('')).forEach(character => reversed = character + reversed);
+  return reversed;
 }
 
 const str = '.uoy fo lla naht ynapmoc retteb a ekam nac I .ynapmoc siht ta OEC eht eb ot evresed I';
