d0a46bf1cd48125ec4b3da9ece9561fbc12408fb
Julie Chung <juliehchung96@gmail.com>
2020-05-04 11:32:03 -0700

    Challenge completed

--- reverse-string/main.js
+++ reverse-string/main.js
@@ -3,10 +3,3 @@ function reverseString(str) {
   (str.split('')).forEach(character => reversed = character + reversed);
   return reversed;
 }
-
-const str = '.uoy fo lla naht ynapmoc retteb a ekam nac I .ynapmoc siht ta OEC eht eb ot evresed I';
-
-const str2 = 'eiluJ ,gnuhC';
-
-
-console.log(reverseString(str2));
