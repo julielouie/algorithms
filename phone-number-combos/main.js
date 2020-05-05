0e8bceebe37736696994c8918f0805e10bf3c8c7
Julie Chung <juliehchung96@gmail.com>
2020-05-05 11:56:00 -0700

    Working through conversion of digit to characters

--- phone-number-combos/main.js
+++ phone-number-combos/main.js
@@ -1,4 +1,34 @@
 var letterCombinations = function (digits) {
+  digits = digits.split('');
+  const chars = [];
+  for (let index = 0; index < digits.length; index++) {
+    switch (digits[index]) {
+      case 2:
+        chars.push('abc');
+        break;
+      case 3:
+        chars.push('def');
+        break;
+      case 4:
+        chars.push('ghi');
+        break;
+      case 5:
+        chars.push('jkl');
+        break;
+      case 6:
+        chars.push('mno');
+        break;
+      case 7:
+        chars.push('pqrs');
+        break;
+      case 8:
+        chars.push('tuv');
+        break;
+      case 9:
+        chars.push('wxyz');
+        break;
+    }
+  }
 
 };
 
