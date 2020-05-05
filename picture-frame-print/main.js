d30194f00d71be8cea166de7718ee8e462a04fc8
Julie Chung <juliehchung96@gmail.com>
2020-05-05 11:28:46 -0700

    Error handling working

--- picture-frame-print/main.js
+++ picture-frame-print/main.js
@@ -34,16 +34,16 @@ function printFrame(width, height, character) {
   return frame;
 }
 
-const width = 10;
-const heigth = 10;
-const character = '#';
+// const width = 10;
+// const heigth = 10;
+// const character = '#';
 
 // const width = 4;
 // const heigth = 5;
 // const character = 'X';
 
-// const width = 2;
-// const heigth = 3;
-// const character = '$';
+const width = 2;
+const heigth = 3;
+const character = '$';
 
 console.log(printFrame(width, heigth, character));
