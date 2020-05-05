99b295eb662afbe51f8608f088251a7ec15407c9
Julie Chung <juliehchung96@gmail.com>
2020-05-05 12:13:22 -0700

    To be continued/revisited

--- phone-number-combos/main.js
+++ phone-number-combos/main.js
@@ -1,34 +1,18 @@
 var letterCombinations = function (digits) {
   digits = digits.split('');
-  const chars = [];
-  for (let index = 0; index < digits.length; index++) {
-    switch (parseInt(digits[index])) {
-      case 2:
-        chars.push('abc');
-        break;
-      case 3:
-        chars.push('def');
-        break;
-      case 4:
-        chars.push('ghi');
-        break;
-      case 5:
-        chars.push('jkl');
-        break;
-      case 6:
-        chars.push('mno');
-        break;
-      case 7:
-        chars.push('pqrs');
-        break;
-      case 8:
-        chars.push('tuv');
-        break;
-      case 9:
-        chars.push('wxyz');
-        break;
-    }
+  const chars = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz'
   }
+  // for (let index = 0; index < digits.length; index++) {
+
+  // }
   console.log(chars)
 
 };
