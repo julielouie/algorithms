538c3ac990d2dfb81804515e62f95aa9d3077db4
Julie Chung <juliehchung96@gmail.com>
2020-05-05 11:16:53 -0700

    Added basic test cases

--- picture-frame-print/main.js
+++ picture-frame-print/main.js
@@ -1,3 +1,17 @@
 function printFrame(width, height, character) {
 
 }
+
+const width = 10;
+const heigth = 10;
+const character = '#';
+
+// const width = 4;
+// const heigth = 5;
+// const character = 'X';
+
+// const width = 2;
+// const heigth = 3;
+// const character = '$';
+
+console.log(printFrame(width, heigth, character));
