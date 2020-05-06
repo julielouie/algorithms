7e4f9451b70cd3b6f35804e74e81c3b2ce6373de
Julie Chung <juliehchung96@gmail.com>
2020-05-06 12:03:04 -0700

    Passes basic test cases

--- count-calories/main.js
+++ count-calories/main.js
@@ -21,10 +21,10 @@ function countSuccessDays(calories, exHours) {
   return successes;
 }
 
-const calories = [123, 456, 48, 685, 8569, 12, 48];
-const exHours = [2, 4, 5, 7, 9, 10, 5];
+// const calories = [123, 456, 48, 685, 8569, 12, 48];
+// const exHours = [2, 4, 5, 7, 9, 10, 5];
 
-// const calories = [2, 5, 1, 8, 4];
-// const exHours = [9, 8, 7, 5, 9];
+const calories = [2, 5, 1, 8, 4];
+const exHours = [9, 8, 7, 5, 9];
 
 console.log(countSuccessDays(calories, exHours));
