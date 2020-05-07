17748c0fca45d04f8c37bbf8ef29da1078c83c00
Julie Chung <juliehchung96@gmail.com>
2020-05-07 16:27:49 -0700

    Starting new challenge, added test cases

---
+++ circular-printer/main.js
@@ -0,0 +1,17 @@
+function calcPrintTime(chars) {
+  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+
+}
+
+const chars = 'BZA';
+  // 4 seconds
+
+// const chars = 'ZGB';
+  // 13 seconds
+
+// const chars = 'YGSL';
+  // 29 seconds
+
+console.log(calcPrintTime(chars));
