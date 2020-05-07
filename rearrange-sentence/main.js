c4ef445f5e39e88ae31e7e37b450a23c995d74bd
Julie Chung <juliehchung96@gmail.com>
2020-05-07 15:17:54 -0700

    Starting new challenge

---
+++ rearrange-sentence/main.js
@@ -0,0 +1,9 @@
+function rearrangeStr(str) {
+  const strArr = str.toLowerCase().split(' ');
+  console.log(strArr)
+}
+
+const sentence = 'The lines are printed in reverse order.';
+ // 'In the lines order printed reverse'
+
+console.log(rearrangeStr(sentence));
