f10ded9831011481f1ea2b0ebde82d0f5743d84e
Julie Chung <juliehchung96@gmail.com>
2020-05-07 16:32:53 -0700

    Found index of alphabet in array

--- circular-printer/main.js
+++ circular-printer/main.js
@@ -1,6 +1,14 @@
 function calcPrintTime(chars) {
   const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let seconds = 0;
+  for (let index = 0; index < chars.length; index++) {
+    const letter = chars[index];
+    const alphIndex = alphabet.indexOf(letter);
 
+    console.log(alphIndex)
+
+
+  }
 
 
 }
