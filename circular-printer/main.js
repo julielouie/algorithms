611429e7d734597d4fadf57f312ee2c3f1af9798
Julie Chung <juliehchung96@gmail.com>
2020-05-07 17:05:37 -0700

    Have all distances, need to calculate together

--- circular-printer/main.js
+++ circular-printer/main.js
@@ -1,18 +1,18 @@
 function calcPrintTime(chars) {
   const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   const length = alphabet.length;
-  let seconds = [];
+  let distances = [];
   for (let index = 0; index < chars.length; index++) {
     const current = chars[index];
     let alphIndex = alphabet.indexOf(current);
     if (alphIndex > (length / 2)) {
       alphIndex = (length - alphIndex) * (-1);
     }
-    seconds.push(alphIndex);
+    distances.push(alphIndex);
   }
-  seconds.sort((a, b) => a - b);
+  console.log(distances)
+  let seconds = distances[0];
   console.log(seconds)
-
 }
 
 const chars = 'BZA';
