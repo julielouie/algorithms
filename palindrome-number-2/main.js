4380827d344b16e0cfc4dcb67d7ae8d083605538
Julie Chung <juliehchung96@gmail.com>
2020-05-09 12:02:16 -0700

    Basic functionality completed

--- palindrome-number-2/main.js
+++ palindrome-number-2/main.js
@@ -1,28 +1,26 @@
 function isNumPalindrome(num) {
   if (num < 0) {
     return;
+  } else {
+    const numStr = `${num}`;
+    const halfLength = numStr.length / 2;
+    for (let index = 0, backInd = numStr.length - 1; index < halfLength, backInd >= halfLength; index++, backInd--) {
+      if (numStr[index] === numStr[backInd]) {
+        continue;
+      } else {
+        return false;
+      }
+    }
   }
-  const numStr = `${num}`;
-  const halfLength = numStr.length / 2;
-  let firstHalf = '';
-  let backHalf = '';
-  for (let index = 0, backInd = numStr.length - 1; index <= halfLength, backInd >= halfLength; index++, backInd--) {
-    firstHalf += numStr[index];
-    backHalf += numStr[backInd];
-  }
-  const lowerPal = firstHalf + firstHalf.split().reverse().join('');
-  const upperPal = backHalf + backHalf.split().reverse().join('');
-  if (numStr === lowerPal) {
-    return num;
-  } else if (parseInt(lowerPal) < num && parseInt(upperPal) > num) {
-    return parseInt(lowerPal);
-  }
+  return num;
 }
 
 // const num = 121;
 
-const num = 199;
+// const num = 199;
 
 // const num = 15;
+
+const num = 11;
 
 console.log(isNumPalindrome(num));
