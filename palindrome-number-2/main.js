eaa7a99cb0aeeb2a2bb1dd28b958aa21bab7587c
Julie Chung <juliehchung96@gmail.com>
2020-05-09 11:18:47 -0700

    Working through check

--- palindrome-number-2/main.js
+++ palindrome-number-2/main.js
@@ -1,9 +1,23 @@
 function isNumPalindrome(num) {
+  if (num < 0) {
+    return;
+  } else {
+    const numStr = `${num}`;
+    for (let index = 0, backInd = numStr.length - 1; index < numStr.length, backInd >= 0; index++, backInd--) {
+      if (numStr[index] === numStr[backInd]) {
+        continue;
+      } else {
 
+      }
+    }
+  }
+  return num;
 }
 
 const num = 121;
 
-const num = 199;
+// const num = 199;
 
-const num = 15;
+// const num = 15;
+
+console.log(isNumPalindrome(num));
