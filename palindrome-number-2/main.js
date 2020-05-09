4f7a7cb578d28387def4269427dba45f80dff71f
Julie Chung <juliehchung96@gmail.com>
2020-05-09 11:51:52 -0700

    Continuing to rework closest pal

--- palindrome-number-2/main.js
+++ palindrome-number-2/main.js
@@ -1,16 +1,21 @@
 function isNumPalindrome(num) {
   if (num < 0) {
     return;
-  } else {
-    const numStr = `${num}`;
-    const halfLength = numStr.length / 2;
-    let firstHalf = '';
-    for (let index = 0, backInd = numStr.length - 1; index <= halfLength, backInd >= halfLength; index++, backInd--) {
-      if (numStr[index] === numStr[backInd]) {
-        firstHalf += numStr[index];
-        continue;
-      } else {
-
+  }
+  const numStr = `${num}`;
+  const halfLength = numStr.length / 2;
+  let firstHalf = '';
+  let backHalf = '';
+  for (let index = 0, backInd = numStr.length - 1; index <= halfLength, backInd >= halfLength; index++, backInd--) {
+    if (numStr[index] === numStr[backInd]) {
+      continue;
+    } else {
+      firstHalf += numStr[index];
+      backHalf += numStr[backInd];
+      const lowerPal = firstHalf + firstHalf.split().reverse().join('');
+      const upperPal = backHalf + backHalf.split().reverse().join('');
+      if (parseInt(lowerPal) < num && parseInt(upperPal) > num) {
+        return lowerPal;
       }
     }
   }
