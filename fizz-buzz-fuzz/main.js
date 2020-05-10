2b4926aa5600db027cc3d1289d2298b7a0edf350
Julie Chung <juliehchung96@gmail.com>
2020-05-10 12:57:35 -0700

    Refactored to use ternary

--- fizz-buzz-fuzz/main.js
+++ fizz-buzz-fuzz/main.js
@@ -1,20 +1,26 @@
 function fizzBuzzFuzz(index) {
   for (let index = 1; index < 151; index++) {
-    if (index % 105 === 0) {
-      console.log('FizzBuzzFuzz');
-    } else if (index % 35 === 0) {
-      console.log('BuzzFuzz');
-    } else if (index % 21 === 0) {
-      console.log('FizzFuzz');
-    } else if (index % 7 === 0) {
-      console.log('Fuzz');
-    } else if (index % 5 === 0) {
-      console.log('Buzz');
-    } else if (index % 3 === 0) {
-      console.log('Fizz');
-    } else {
-      console.log(index);
-    }
+    console.log(
+      (index % 3 ? '' : 'Fizz') +
+      (index % 5 ? '' : 'Buzz') +
+      (index % 7 ? '' : 'Fuzz') +
+      || index
+      );
+    // if (index % 105 === 0) {
+    //   console.log('FizzBuzzFuzz');
+    // } else if (index % 35 === 0) {
+    //   console.log('BuzzFuzz');
+    // } else if (index % 21 === 0) {
+    //   console.log('FizzFuzz');
+    // } else if (index % 7 === 0) {
+    //   console.log('Fuzz');
+    // } else if (index % 5 === 0) {
+    //   console.log('Buzz');
+    // } else if (index % 3 === 0) {
+    //   console.log('Fizz');
+    // } else {
+    //   console.log(index);
+    // }
   }
 }
 
