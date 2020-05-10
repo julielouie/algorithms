890c1c1f0e2d02f0eccd36bb6eafca875fa4b57b
Julie Chung <juliehchung96@gmail.com>
2020-05-10 12:58:39 -0700

    Refactoring completed

--- fizz-buzz-fuzz/main.js
+++ fizz-buzz-fuzz/main.js
@@ -6,21 +6,6 @@ function fizzBuzzFuzz(index) {
       (index % 7 ? '' : 'Fuzz')
       || index
       );
-    // if (index % 105 === 0) {
-    //   console.log('FizzBuzzFuzz');
-    // } else if (index % 35 === 0) {
-    //   console.log('BuzzFuzz');
-    // } else if (index % 21 === 0) {
-    //   console.log('FizzFuzz');
-    // } else if (index % 7 === 0) {
-    //   console.log('Fuzz');
-    // } else if (index % 5 === 0) {
-    //   console.log('Buzz');
-    // } else if (index % 3 === 0) {
-    //   console.log('Fizz');
-    // } else {
-    //   console.log(index);
-    // }
   }
 }
 
