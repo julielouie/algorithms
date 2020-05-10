8c8947f8c019bab2db45c88bae6937efcc1aa47d
Julie Chung <juliehchung96@gmail.com>
2020-05-10 12:51:47 -0700

    Removed unnecessary test cases

--- fizz-buzz-fuzz/main.js
+++ fizz-buzz-fuzz/main.js
@@ -18,11 +18,4 @@ function fizzBuzzFuzz(index) {
   }
 }
 
-const num = 3;
-// const num = 5;
-// const num = 7;
-// const num = 21;
-// const num = 35;
-// const num = 105;
-
 console.log(fizzBuzzFuzz(num));
