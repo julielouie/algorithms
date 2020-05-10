691198f7318bee8e9e8818435fcdb0dd40ea0ccf
Julie Chung <juliehchung96@gmail.com>
2020-05-10 12:44:13 -0700

    Added basic test cases

---
+++ fizz-buzz-fuzz/main.js
@@ -0,0 +1,12 @@
+function fizzBuzzFuzz(num) {
+
+}
+
+const num = 3;
+// const num = 5;
+// const num = 7;
+// const num = 21;
+// const num = 35;
+// const num = 105;
+
+console.log(fizzBuzzFuzz(num));
