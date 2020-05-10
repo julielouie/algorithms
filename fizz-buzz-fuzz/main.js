04dfc5f2ed5ca8b50e0098bdaeab805b2e4c37ce
Julie Chung <juliehchung96@gmail.com>
2020-05-10 12:49:57 -0700

    Completed conditionals

--- fizz-buzz-fuzz/main.js
+++ fizz-buzz-fuzz/main.js
@@ -1,6 +1,20 @@
 function fizzBuzzFuzz(num) {
   for (let index = 1; index < 151; index++) {
-
+    if (num % 105 === 0) {
+      return 'FizzBuzzFuzz';
+    } else if (num % 35 === 0) {
+      return 'BuzzFuzz';
+    } else if (num % 21 === 0) {
+      return 'FizzFuzz';
+    } else if (num % 7 === 0) {
+      return 'Fuzz';
+    } else if (num % 5 === 0) {
+      return 'Buzz';
+    } else if (num % 3 === 0) {
+      return 'Fizz';
+    } else {
+      return num;
+    }
   }
 }
 
