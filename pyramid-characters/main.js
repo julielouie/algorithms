15ab21316f0a37ecbabd7d25c73400ef9440ce20
Julie Chung <juliehchung96@gmail.com>
2020-05-11 15:48:35 -0700

    Figured working solution without recursion

--- pyramid-characters/main.js
+++ pyramid-characters/main.js
@@ -1,13 +1,22 @@
 function buildPyramid(X) {
   if (X < 1) return 'Please enter a positive number!';
 
-  const baseCharCount = 2 * (X - 1) + 1;
-  buildPyramid(X - 1);
+  let char = 1
+  let spaces = X;
 
-  const spCount = baseCharCount - (baseCharCount - 2);
-  console.log(spCount)
+  for (let index = 0; spaces > 0; index++) {
+    console.log(" ".repeat(spaces), "#".repeat(char))
+    char += 2
+    spaces -= 1
+  }
 
-  console.log('#'.repeat(baseCharCount));
+  // const baseCharCount = 2 * (X - 1) + 1;
+  // buildPyramid(X - 1);
+
+  // const spCount = baseCharCount - (baseCharCount - 2);
+  // console.log(spCount)
+
+  // console.log('#'.repeat(baseCharCount));
 }
 
 // const num = 2;
