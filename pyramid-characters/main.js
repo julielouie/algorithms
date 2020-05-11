8e61f52beb2fc87b1ec958c73593e542dc91916b
Julie Chung <juliehchung96@gmail.com>
2020-05-11 16:15:14 -0700

    Will revisit recursiveness later, completed functionality for now

--- pyramid-characters/main.js
+++ pyramid-characters/main.js
@@ -5,22 +5,16 @@ function buildPyramid(X) {
   let spaces = X;
 
   for (let index = 0; spaces > 0; index++) {
-    console.log(" ".repeat(spaces), "#".repeat(char))
-    char += 2
-    spaces -= 1
+    console.log(' '.repeat(spaces), '#'.repeat(char))
+    char += 2;
+    spaces -= 1;
   }
 
-  // const baseCharCount = 2 * (X - 1) + 1;
-  // buildPyramid(X - 1);
-
-  // const spCount = baseCharCount - (baseCharCount - 2);
-  // console.log(spCount)
-
-  // console.log('#'.repeat(baseCharCount));
 }
 
+const num = 0;
 // const num = 2;
-const num = 5;
+// const num = 5;
 // const num = 10;
 
 console.log(buildPyramid(num));
