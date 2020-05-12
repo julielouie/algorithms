c4a0a17ebfbd7b2801813a065e1689b4fba2118e
Julie Chung <juliehchung96@gmail.com>
2020-05-12 10:40:17 -0700

    Added basic test cases

--- create-queue/main.js
+++ create-queue/main.js
@@ -11,3 +11,15 @@ class Queue {
     this.data.pop();
   }
 }
+
+const queuie = new Queue();
+
+queuie.add(1);
+queuie.add(2);
+queuie.add(3);
+queuie.add(4);
+queuie.add(5);
+console.log(queuie);
+// queuie.remove();
+// queuie.remove();
+// console.log(queuie);
