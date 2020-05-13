44c6d31ab5cea9efdae6b861ecbfa6590c34c145
Julie Chung <juliehchung96@gmail.com>
2020-05-13 11:27:50 -0700

    Attempted refactor

--- is-palindrome/main.js
+++ is-palindrome/main.js
@@ -1,21 +1,21 @@
 function isPalindrome(str) {
   str = str.toLowerCase().split(' ').join('');
-  const halfLen = str.length / 2;
-  for (let index = 0, backInd = str.length - 1;
-    index <= halfLen, backInd >= halfLen;
-    index++, backInd--) {
-    if (str[index] !== str[backInd]) {
-      return false;
-    }
-  }
-  return true;
+  // const halfLen = str.length / 2;
+  // for (let index = 0, backInd = str.length - 1;
+  //   index <= halfLen, backInd >= halfLen;
+  //   index++, backInd--) {
+  //   if (str[index] !== str[backInd]) {
+  //     return false;
+  //   }
+  // }
+  // return true;
 }
 
-const str = 'noon';
+// const str = 'noon';
 // const str = 'Mom';
 // const str = 'Kayak';
 // const str = 'racecaR';
-// const str = 'Was it a cat I saw';
+const str = 'Was it a cat I saw';
 // const str = 'Not a palindrome';
 
 console.log(isPalindrome(str));
