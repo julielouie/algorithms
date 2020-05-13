bbbdf5a9af090b383fbb385529351d566ca04865
Julie Chung <juliehchung96@gmail.com>
2020-05-13 11:18:47 -0700

    Working through solution

--- is-palindrome/main.js
+++ is-palindrome/main.js
@@ -1,14 +1,26 @@
 function isPalindrome(str) {
-  const condensed = str.toLowerCase().split(' ');
-  const reversed = condensed.reverse().join('');
-  if (reversed === condensed.join('')) return true;
-  return false;
+  str = str.toLowerCase().split(' ').join('');
+  console.log(str)
+  const halfLen = str.length / 2;
+  console.log(halfLen)
+  for (let index = 0, backInd = str.length - 1;
+    index <= halfLen, halfLen >= backInd;
+    index++, backInd--) {
+    console.log(str[index], str[backInd]);
+    if (str[index] === str[backInd]) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
 }
 
-const str = 'noon';
+// const str = 'noon';
 // const str = 'Mom';
 // const str = 'Kayak';
 // const str = 'racecaR';
 // const str = 'Was it a cat I saw';
+const str = 'Not a palindrome';
 
 console.log(isPalindrome(str));
