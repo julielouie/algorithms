c8b547bc2a02f1efa192a98a1c2d76f288e2753a
Julie Chung <juliehchung96@gmail.com>
2020-05-13 11:20:28 -0700

    Passes basic test cases

--- is-palindrome/main.js
+++ is-palindrome/main.js
@@ -1,12 +1,9 @@
 function isPalindrome(str) {
   str = str.toLowerCase().split(' ').join('');
-  console.log(str)
   const halfLen = str.length / 2;
-  console.log(halfLen)
   for (let index = 0, backInd = str.length - 1;
-    index <= halfLen, halfLen >= backInd;
+    index <= halfLen, backInd >= halfLen;
     index++, backInd--) {
-    console.log(str[index], str[backInd]);
     if (str[index] !== str[backInd]) {
       return false;
     }
@@ -14,11 +11,11 @@ function isPalindrome(str) {
   return true;
 }
 
-// const str = 'noon';
+const str = 'noon';
 // const str = 'Mom';
 // const str = 'Kayak';
 // const str = 'racecaR';
 // const str = 'Was it a cat I saw';
-const str = 'Not a palindrome';
+// const str = 'Not a palindrome';
 
 console.log(isPalindrome(str));
