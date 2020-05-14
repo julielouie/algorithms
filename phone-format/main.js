e263c474b6954c98cd35e2a80d227fd88707c1e9
Julie Chung <juliehchung96@gmail.com>
2020-05-14 14:52:02 -0700

    Completed functionality

--- phone-format/main.js
+++ phone-format/main.js
@@ -1,5 +1,9 @@
 function formatPhoneNumber(phoneNum) {
-
+  const phoneArr = `${phoneNum}`.split('');
+  phoneArr.unshift('(');
+  phoneArr.splice(4, 0, ') ');
+  phoneArr.splice(8, 0, '-');
+  return phoneArr.join('');
 }
 
 const nums = 1234567891;
