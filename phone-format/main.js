ace82f0007ef58b3bb606475eba10151ea69d279
Julie Chung <juliehchung96@gmail.com>
2020-05-14 14:47:19 -0700

    added basic test cases

---
+++ phone-format/main.js
@@ -0,0 +1,10 @@
+function formatPhoneNumber(phoneNum) {
+
+}
+
+const nums = 1234567891;
+// const nums = 1534267894;
+// const nums = 1856423985;
+// const nums = 8527419630;
+
+console.log(formatPhoneNumber(nums));
