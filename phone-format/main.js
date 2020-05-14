7a6c50df274a0cc9df061793412cecfad290e28f
Julie Chung <juliehchung96@gmail.com>
2020-05-14 14:53:53 -0700

    Passed all basic test cases

--- phone-format/main.js
+++ phone-format/main.js
@@ -6,9 +6,9 @@ function formatPhoneNumber(phoneNum) {
   return phoneArr.join('');
 }
 
-const nums = 1234567891;
+// const nums = 1234567891;
 // const nums = 1534267894;
 // const nums = 1856423985;
-// const nums = 8527419630;
+const nums = 8527419630;
 
 console.log(formatPhoneNumber(nums));
