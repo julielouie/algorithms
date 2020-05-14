6ad70ee33a7bd29065263c051a0f7a2353c8c53a
Julie Chung <juliehchung96@gmail.com>
2020-05-14 14:54:35 -0700

    Challenge completed

--- phone-format/main.js
+++ phone-format/main.js
@@ -5,10 +5,3 @@ function formatPhoneNumber(phoneNum) {
   phoneArr.splice(8, 0, '-');
   return phoneArr.join('');
 }
-
-// const nums = 1234567891;
-// const nums = 1534267894;
-// const nums = 1856423985;
-const nums = 8527419630;
-
-console.log(formatPhoneNumber(nums));
