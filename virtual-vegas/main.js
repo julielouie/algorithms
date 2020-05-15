494ede7d423b50c217daf26c82d4887a90d05c36
Julie Chung <juliehchung96@gmail.com>
2020-05-15 12:43:39 -0700

    Funtionality completed

--- virtual-vegas/main.js
+++ virtual-vegas/main.js
@@ -1,12 +1,12 @@
 function isJackpot(slotArr) {
-
+  return slotArr.every(slotItem => slotItem === slotArr[0]);
 }
 
 
-const items = ["@", "@", "@", "@"];
+// const items = ["@", "@", "@", "@"];
 // const items = ["!", "!", "!", "!"];
 // const items = ["*", "*", "*", "*"];
 // const items = ["/", "/", "@", "@"];
-// const items = ["*", "*", "*", "@"];
+const items = ["*", "*", "*", "@"];
 
 console.log(isJackpot(items));
