cb31b15400363a594c85d27a7e2d83b504d34464
Julie Chung <juliehchung96@gmail.com>
2020-05-15 12:59:03 -0700

    Added basic test cases

--- virtual-vegas/statue-size/main.js
+++ virtual-vegas/statue-size/main.js
@@ -2,8 +2,12 @@ function makeArrayConsecutive2(statues) {
   const sorted = statues.sort((a, b) => a - b);
 
   let count = 0;
-  let currentVal = null;
-
-
+  let currentVal = sorted[0];
 
 }
+
+const statues = [6, 2, 3, 8]; // 3
+// const statues = [1, 2, 4, 6]; // 2
+// const statues = [5, 8, 6, 3, 1, 4, 12]; // 6
+
+console.log(makeArrayConsecutive2(statues));
