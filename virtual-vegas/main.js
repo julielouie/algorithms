03e93a954e94d82b3a2cd5f8c361689a74abe180
Julie Chung <juliehchung96@gmail.com>
2020-05-15 12:44:51 -0700

    Challenge completed

--- virtual-vegas/main.js
+++ virtual-vegas/main.js
@@ -1,12 +1,3 @@
 function isJackpot(slotArr) {
   return slotArr.every(slotItem => slotItem === slotArr[0]);
 }
-
-
-// const items = ["@", "@", "@", "@"];
-// const items = ["!", "!", "!", "!"];
-// const items = ["*", "*", "*", "*"];
-// const items = ["/", "/", "@", "@"];
-const items = ["*", "*", "*", "@"];
-
-console.log(isJackpot(items));
