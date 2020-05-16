147e9382cf626c8ece02ebde9e2d0ca9ff551200
Julie Chung <juliehchung96@gmail.com>
2020-05-16 13:15:34 -0700

    Completed functionality

--- phone-format-2/main.js
+++ phone-format-2/main.js
@@ -1,18 +1,20 @@
 function formatPhone(phoneNums) {
-  const map = {
-    ABC: 2,
-    DEF: 3,
-    GHI: 4,
-    JKL: 5,
-    MNO: 6,
-    PQRS: 7,
-    TUV: 8,
-    WXYZ: 9
-  }
+  const map = ['none', 'none', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+  // const map = {
+  //   ABC: 2,
+  //   DEF: 3,
+  //   GHI: 4,
+  //   JKL: 5,
+  //   MNO: 6,
+  //   PQRS: 7,
+  //   TUV: 8,
+  //   WXYZ: 9
+  // }
 
   const matchMap = match => {
-    console.log(match)
-    match = map[match]
+    let num = 0;
+    map.forEach((chars, index) => chars.includes(match) ? num = index : 0);
+    return num;
   };
   const newNums = phoneNums.replace(/([A-Z])/g, matchMap);
   console.log(newNums)
