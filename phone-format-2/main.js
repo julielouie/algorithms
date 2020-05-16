a571deb0bc9fba6a09d2b23366cd2b52a6ed5254
Julie Chung <juliehchung96@gmail.com>
2020-05-16 11:52:44 -0700

    Added basic test cases

--- phone-format-2/main.js
+++ phone-format-2/main.js
@@ -12,3 +12,10 @@ function formatPhone(phoneNums) {
     9: 'WXYZ'
   }
 }
+
+const nums = '800-YOU-RANG';
+// const nums = '800-588-2300';
+// const nums = '234-NOT-HERE';
+// const nums = '751-LOL-8274';
+
+console.log(formatPhone(nums));
