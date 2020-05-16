73c8540f4765b1f73541e87bde5e06a7a3d82b90
Julie Chung <juliehchung96@gmail.com>
2020-05-16 11:50:52 -0700

    Starting function

---
+++ phone-format-2/main.js
@@ -0,0 +1,14 @@
+function formatPhone(phoneNums) {
+  const map = {
+    0: 'none',
+    1: 'none',
+    2: 'ABC',
+    3: 'DEF',
+    4: 'GHI',
+    5: 'JKL',
+    6: 'MNO',
+    7: 'PQRS',
+    8: 'TUV',
+    9: 'WXYZ'
+  }
+}
