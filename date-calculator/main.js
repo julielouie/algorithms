b8d03b652d62417ba68ea624acece45e20f0ce4f
Julie Chung <juliehchung96@gmail.com>
2020-05-17 11:42:38 -0700

    Challenge completed

--- date-calculator/main.js
+++ date-calculator/main.js
@@ -4,13 +4,3 @@ function daysToFreedom(date1, date2) {
   const timeDifference = end.getTime() - start.getTime();
   return timeDifference / (1000 * 60 * 60 * 24); //num of milliseconds in a day
 }
-
-const date1 = '3/19/20';
-
-// const date2 = '5/15/20';
-// const date2 = '5/30/20';
-// const date2 = '6/15/20';
-// const date2 = '6/30/20';
-const date2 = '10/01/20';
-
-console.log(daysToFreedom(date1, date2));
