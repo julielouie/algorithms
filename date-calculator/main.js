372b8dfac4f3eeaf505fad1c12272bde64955d31
Julie Chung <juliehchung96@gmail.com>
2020-05-17 11:40:50 -0700

    Basic functionality completed

--- date-calculator/main.js
+++ date-calculator/main.js
@@ -1,5 +1,8 @@
 function daysToFreedom(date1, date2) {
-
+  const start = new Date(date1);
+  const end = new Date(date2);
+  const timeDifference = end.getTime() - start.getTime();
+  return timeDifference / (1000 * 60 * 60 * 24); //num of milliseconds in a day
 }
 
 const date1 = '3/19/20';
