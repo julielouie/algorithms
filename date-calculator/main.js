1fbb508e28292a6d92bc441d1baaa560789b2240
Julie Chung <juliehchung96@gmail.com>
2020-05-17 11:41:23 -0700

    Passes basic test cases

--- date-calculator/main.js
+++ date-calculator/main.js
@@ -7,10 +7,10 @@ function daysToFreedom(date1, date2) {
 
 const date1 = '3/19/20';
 
-const date2 = '5/15/20';
+// const date2 = '5/15/20';
 // const date2 = '5/30/20';
 // const date2 = '6/15/20';
 // const date2 = '6/30/20';
-// const date2 = '10/01/20';
+const date2 = '10/01/20';
 
 console.log(daysToFreedom(date1, date2));
