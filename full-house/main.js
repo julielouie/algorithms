90db2593ab152672c3f20480a6c0134ea23b99c7
Julie Chung <juliehchung96@gmail.com>
2020-05-18 14:36:44 -0700

    Challenge completed

--- full-house/main.js
+++ full-house/main.js
@@ -3,12 +3,3 @@ function isFullHouse(handArr) {
   const split = [sorted.slice(0, 2), sorted.slice(2, 5)];
   return (new Set(split[0]).size === 1) && (new Set(split[1]).size === 1);
 }
-
-
-const hand = ["K", "K", "A", "K", "A"]; // true
-// const hand = ["10", "10", "2", "10", "2"]; // true
-// const hand = ["K", "K", "2", "K", "2"]; // true
-// const hand = ["A", "J", "10", "3", "3"]; // false
-// const hand = ["A", "J", "J", "J", "J"]; // false
-
-console.log(isFullHouse(hand));
