074230cb1f44b744030f6c6d6d588961a9fc7006
Julie Chung <juliehchung96@gmail.com>
2020-05-18 14:31:50 -0700

    Refactored conditional

--- full-house/main.js
+++ full-house/main.js
@@ -1,11 +1,5 @@
 function isFullHouse(handArr) {
-  let sorted;
-  const nums = !handArr.some(isNaN);
-  if (nums) {
-    sorted = handArr.sort((a, b) => a - b);
-  } else {
-    sorted = handArr.sort();
-  }
+  let sorted = !handArr.some(isNaN) ? handArr.sort((a, b) => a - b) : handArr.sort();
   const split = [sorted.slice(0, 2), sorted.slice(2, 5)];
   const check1 = new Set(split[0]).size === 1;
   const check2 = new Set(split[1]).size === 1;
@@ -15,8 +9,8 @@ function isFullHouse(handArr) {
 
 
 // const hand = ["K", "K", "A", "K", "A"]; // true
-const hand = ["10", "10", "2", "10", "2"]; // true
+// const hand = ["10", "10", "2", "10", "2"]; // true
 // const hand = ["K", "K", "2", "K", "2"]; // true
-// const hand = ["A", "J", "10", "3", "3"]; // false
+const hand = ["A", "J", "10", "3", "3"]; // false
 
 console.log(isFullHouse(hand));
