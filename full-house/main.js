858734cf13729f90a884a2be1eccb709ee828e02
Julie Chung <juliehchung96@gmail.com>
2020-05-18 10:32:10 -0700

    Completed functionality

--- full-house/main.js
+++ full-house/main.js
@@ -1,5 +1,10 @@
 function isFullHouse(handArr) {
-
+  const sorted = handArr.sort();
+  const split = [sorted.slice(0, 2), sorted.slice(2, 5)];
+  const check1 = new Set(split[0]).size === 1;
+  const check2 = new Set(split[1]).size === 1;
+  if (check1 && check2) return true;
+  return false;
 }
 
 
