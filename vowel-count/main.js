02f6ca05af224f30a727192099abea00efc75228
Julie Chung <juliehchung96@gmail.com>
2020-05-20 12:31:14 -0700

    Added basic test cases

---
+++ vowel-count/main.js
@@ -0,0 +1,10 @@
+function countVowels(str) {
+  let count = 0;
+
+}
+
+const str = "supercalifragilisticexpialidocious"; // 16
+// const str = 'aeiouy'; // 5
+// const str = 'nschthng' // 0
+
+console.log(countVowels(str));
