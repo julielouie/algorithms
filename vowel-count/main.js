9947dedf16bbf0dd271a15beab26e9e2596aafed
Julie Chung <juliehchung96@gmail.com>
2020-05-20 12:37:58 -0700

    Completed code

--- vowel-count/main.js
+++ vowel-count/main.js
@@ -5,9 +5,3 @@ function countVowels(str) {
   });
   return count;
 }
-
-// const str = "supercalifragilisticexpialidocious"; // 16
-// const str = 'aeiouy'; // 5
-const str = 'nschthng' // 0
-
-console.log(countVowels(str));
