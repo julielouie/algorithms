48ba3fa58e94eb723d1bbacc6337feb6de2c4fad
Julie Chung <juliehchung96@gmail.com>
2020-05-21 10:35:24 -0700

    Functionality completed

--- fly-swatter/main.js
+++ fly-swatter/main.js
@@ -5,10 +5,3 @@ function removeFlys(str) {
   }
   return 'No flies here!';
 }
-
-// const str = "flyflyfflllgoflyyyyflynefffff"; // 'gone'
-// const str = 'dragonfly'; // 'dragon'
-// const str = 'stupefy'; // 'stupe'
-const str = 'remove'; // 'No flies here!'
-
-console.log(removeFlys(str));
