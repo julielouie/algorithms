c233a95e112c3812de555f79166ba66036cd5fc5
Julie Chung <juliehchung96@gmail.com>
2020-05-21 10:09:03 -0700

    Added basic test cases

--- fly-swatter/main.js
+++ fly-swatter/main.js
@@ -1,3 +1,10 @@
 function removeFlys(str) {
 
 }
+
+const str = "flyflyfflllgoflyyyyflynefffff"; // 'gone'
+// const str = 'dragonfly'; // 'dragon'
+// const str = 'stupefy'; // 'stupe'
+// const str = 'remove'; // 'No flies here!'
+
+console.log(removeFlys(str));
