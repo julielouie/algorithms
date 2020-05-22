d190975e0090ee6d0acb5bb790cbfb0fd4f34b8e
Julie Chung <juliehchung96@gmail.com>
2020-05-22 12:46:16 -0700

    Added basic test cases

---
+++ bubble-sort/main.js
@@ -0,0 +1,9 @@
+function bubbleSort(numsArr) {
+
+}
+
+const nums = [8, 4, 3, 2, 5, 7, 1, 98];
+// const nums = [45, 48, 13, 19, 27, 65];
+// const nums = [85, 495, 65, 78, 26, 0];
+
+console.log(bubbleSort(nums));
