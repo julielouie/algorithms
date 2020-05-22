66c1a1d221368056411acf31b8b7258b50326588
Julie Chung <juliehchung96@gmail.com>
2020-05-22 12:57:53 -0700

    Passes all basic test cases

--- bubble-sort/main.js
+++ bubble-sort/main.js
@@ -13,8 +13,8 @@ function bubbleSort(numsArr) {
   return numsArr;
 }
 
-const nums = [8, 4, 3, 2, 5, 7, 1, 98];
+// const nums = [8, 4, 3, 2, 5, 7, 1, 98];
 // const nums = [45, 48, 13, 19, 27, 65];
-// const nums = [85, 495, 65, 78, 26, 0];
+const nums = [85, 495, 65, 78, 26, 0];
 
 console.log(bubbleSort(nums));
