c8eb6853713204e468dee6fcf2ae348741f37bdf
Julie Chung <juliehchung96@gmail.com>
2020-05-25 13:44:40 -0700

    Remove test case exp

--- reverse-binary/main.js
+++ reverse-binary/main.js
@@ -9,9 +9,4 @@ function reversedBinaryInteger(num) {
 // const num = 25; //19
 const num = 45; //45
 
-// 10 = 1010 -> 0101 = 5
-// 12 = 1100 -> 0011 = 3
-// 25 = 11001 -> 10011 = 19
-// 45 = 101101 -> 101101 = 45
-
 console.log(reversedBinaryInteger(num));
