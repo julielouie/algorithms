2851843ce3fac7f77d0b47d37feb68d4214bac82
Julie Chung <juliehchung96@gmail.com>
2020-05-25 13:37:40 -0700

    Added basic test cases

---
+++ reverse-binary/main.js
@@ -0,0 +1,16 @@
+function reversedBinaryInteger(num) {
+
+}
+
+
+const num = 10; //5
+// const num = 12; //3
+// const num = 25; //19
+// const num = 45; //45
+
+// 10 = 1010 -> 0101 = 5
+// 12 = 1100 -> 0011 = 3
+// 25 = 11001 -> 10011 = 19
+// 45 = 101101 -> 101101 = 45
+
+console.log(reversedBinaryInteger(num));
