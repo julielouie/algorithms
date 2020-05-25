7fae057b257637c1289f0405d7b4ffb75e3ff994
Julie Chung <juliehchung96@gmail.com>
2020-05-25 13:45:58 -0700

    Passed all test cases

--- reverse-binary/main.js
+++ reverse-binary/main.js
@@ -3,10 +3,3 @@ function reversedBinaryInteger(num) {
   const reversed = bin.split('').reverse().join('');
   return parseInt(reversed, 2);
 }
-
-// const num = 10; //5
-// const num = 12; //3
-// const num = 25; //19
-const num = 45; //45
-
-console.log(reversedBinaryInteger(num));
