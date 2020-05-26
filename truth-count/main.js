b1af54a779a5854a49a9959cfd8e66628c7feb19
Julie Chung <juliehchung96@gmail.com>
2020-05-26 12:06:32 -0700

    Passed all test cases

--- truth-count/main.js
+++ truth-count/main.js
@@ -3,9 +3,3 @@ function countTrue(arr) {
   arr.forEach(element => element ? count++ : null);
   return count;
 }
-
-// const array = [true, false, false, true, false]; // 2
-// const array = [false, false, false, false]; // 0
-const array = []; // 0
-
-console.log(countTrue(array));
