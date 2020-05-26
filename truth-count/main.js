5ae788e6424308b4fddf79fcdcf940147853c866
Julie Chung <juliehchung96@gmail.com>
2020-05-26 12:03:45 -0700

    Added basic test cases

--- truth-count/main.js
+++ truth-count/main.js
@@ -1,3 +1,9 @@
 function countTrue(array) {
 
 }
+
+const array = [true, false, false, true, false]; // 2
+// const array = [false, false, false, false]; // 0
+// const array = []; // 0
+
+console.log(countTrue(array));
